9ba43131b023135efa0dd1bf607d39c2dfc8043e
zweiein <evangelinathanasiakittymcdowel@gmail.com>
2016-12-26 19:27:24 +0800

    PLSA

--- Probability Lantent Semantic Analysis/Main.py
+++ Probability Lantent Semantic Analysis/Main.py
@@ -21,20 +21,23 @@ for docName, content in doc_wc_dict.items():
 			bg_word[word] = int(frequency)
 
 # initialize P(T|D)
+print "Initialize P(T|D)"
 for docName, wordCount in doc_wc_dict.items():
 	topic_prob = {}
 	for topic, wordProb in topic_word_prob_dict.items():
 		topic_prob[topic] = INIT_PROBABILITY
 	doc_topic_prob_dict[docName] = topic_prob
 
+print "Initialize P(T| w, D)"
 for docName, wordCount in doc_wc_dict.items():	
 	word_list = {}
-	for word, frequency in bg_word.items():	
+	for word, frequency in wordCount.items():	
 		topic_prob = {}
 		for topic, wordProb in topic_word_prob_dict.items():
 			topic_prob[topic] = 0.0
 		word_list[word] = topic_prob
 	doc_word_topic_prob_dict[docName] = word_list
-	
+
+print "start PLSA"	
 [topic_word_prob_dict, doc_topic_prob_dict, doc_wc_dict] = PLSA.Probability_LSA(bg_word, doc_wc_dict, doc_topic_prob_dict, topic_word_prob_dict, doc_word_topic_prob_dict)
 	
