29f936bc02f7a12cbb58e979f68ccca639722050
zweiein <evangelinathanasiakittymcdowel@gmail.com>
2016-12-26 18:25:54 +0800

    PLSA ver1.0

--- Probability Lantent Semantic Analysis/Main.py
+++ Probability Lantent Semantic Analysis/Main.py
@@ -1,9 +1,11 @@
 import ProcDoc
+import PLSA
 import codecs
 
 INIT_PROBABILITY = 1.0 / 60
 topic_word_prob_dict = ProcDoc.read_clusters()		# read cluster P(W|T), {T: {W:Prob}}
 doc_topic_prob_dict = {}							# P(T|D),{D:{T:Prob}} 
+doc_word_topic_prob_dict = {}						# P(T| w, D), {D: {word:{T:prob}}}
 doc_wc_dict = ProcDoc.read_doc_dict()  				# read document (Doc No.,Doc content)  
 bg_word = {}  										# background (word, count)
 
@@ -18,5 +20,21 @@ for docName, content in doc_wc_dict.items():
 		else:
 			bg_word[word] = int(frequency)
 
+# initialize P(T|D)
 for docName, wordCount in doc_wc_dict.items():
+	topic_prob = {}
 	for topic, wordProb in topic_word_prob_dict.items():
+		topic_prob[topic] = INIT_PROBABILITY
+	doc_topic_prob_dict[docName] = topic_prob
+
+for docName, wordCount in doc_wc_dict.items():	
+	word_list = {}
+	for word, frequency in bg_word.items():	
+		topic_prob = {}
+		for topic, wordProb in topic_word_prob_dict.items():
+			topic_prob[topic] = 0.0
+		word_list[word] = topic_prob
+	doc_word_topic_prob_dict[docName] = word_list
+	
+[topic_word_prob_dict, doc_topic_prob_dict, doc_wc_dict] = PLSA.Probability_LSA(bg_word, doc_wc_dict, doc_topic_prob_dict, topic_word_prob_dict, doc_word_topic_prob_dict)
+	
