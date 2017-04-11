472bd9a0b15f41e6167ca1b923bfae3a558ca662
zweiein <evangelinathanasiakittymcdowel@gmail.com>
2017-04-11 13:28:21 +0800

    kl divergence

--- Significant Words Language Models/ProcDoc.py
+++ Significant Words Language Models/ProcDoc.py
@@ -4,34 +4,28 @@ import io
 import os
 import fileinput
 import collections
+import numpy as np
 from math import exp
+from webbrowser import BackgroundBrowser
 
 bg_modle_path = "../Corpus/background"
 Cluster_path = "Topic"
 
+
 # read cluster
-def read_clusters():
-	clusters = {}
-	# cluster_only_utf8
-	for cluster_item in os.listdir(Cluster_path):
+def read_file(filepath):
+	data = {}				# content of document (doc, content)
+	# list all files of a directory(Document)
+	for dir_item in os.listdir(filepath):
 		# join dir path and file name
-		cluster_item_path = os.path.join(Cluster_path, cluster_item)
+		dir_item_path = os.path.join(filepath, dir_item)
 		# check whether a file exists before read
-		if os.path.isfile(cluster_item_path):
-			with io.open(cluster_item_path, 'r', encoding = 'utf8') as f:
-				# read content of query document (doc, content)
-				content =  f.readlines()
-				words = (content[0]).split(",")
-				for line in content[1:]:
-					cluster_info = line.split(",")
-					cluster_name = cluster_info[0]
-					word_prob_dict = {}
-					for w_index in range(1, len(cluster_info)):
-						[word, prob ]= [words[w_index], cluster_info[w_index]]
-						word_prob_dict[word] = float(prob)
-					clusters[cluster_name] = word_prob_dict
-	# clusters(list)
-	return clusters	
+		if os.path.isfile(dir_item_path):
+			with open(dir_item_path, 'r') as f:
+				# read content of document (doc, content)
+				data[dir_item] = f.read()
+	# data(dict)
+	return data	
 	
 # read document
 def read_background_dict():
@@ -72,13 +66,9 @@ def doc_preprocess(dictionary):
 			content += word + " "
 		# replace old content
 		dictionary[key]	= content
-	# term probablity(word_count / word sum)	
+	# term probability(word_count / word sum)	
 	for doc_key, doc_content in dictionary.items():
-		doc_words = {}
-		doc_words = word_count(doc_content, doc_words)
-		doc_words_sum = word_sum(doc_words) * 1.0
-		for word, word_val  in doc_words.items():
-			doc_words[word] = word_val / doc_words_sum
+		doc_words = word_count(doc_content, {})
 		dictionary[doc_key] = doc_words
 		
 	return dictionary
@@ -97,8 +87,29 @@ def query_preprocess(dictionary):
 			content += word + " "
 		# replace old content
 		dictionary[key]	= content
-	return dictionary	
-	
+	return dictionary
+
+def unigram(topic_wordcount_dict):
+	topic_wordprob_dict = {}
+	for topic, wordcount in topic_wordcount_dict.items():
+		length = 1.0 * word_sum(wordcount)
+		word_prob = {}
+		for word, count in wordcount.items():
+			word_prob[word] = count / length
+		topic_wordprob_dict[topic] = word_prob
+	topic_wordprob_dict = collections.OrderedDict(sorted(topic_wordprob_dict.items()))	
+	return topic_wordprob_dict 
+
+def modeling(topic_wordprob_dict, background_model, alpha):
+	modeling_dict = {}
+	for topic, wordprob in topic_wordprob_dict.items():
+		word_model = {}
+		for word in wordprob.keys():
+			word_model[word] = (1-alpha) * wordprob[word] + (alpha) * background_model[word]
+		modeling_dict[topic] = word_model
+	modeling_dict = collections.OrderedDict(sorted(modeling_dict.items()))		
+	return modeling_dict
+
 # word count
 def word_count(content, bg_word):
 	for part in content.split():
--- Significant Words Language Models/main.py
+++ Significant Words Language Models/main.py
@@ -7,93 +7,63 @@ import numpy as np
 import readAssessment
 import ProcDoc
 
-
-documant_path = "../Corpus/SPLIT_DOC_WDID_NEW"
-query_path = "../Corpus/QUERY_WDID_NEW_middle"
 data = {}				# content of document (doc, content)
-background_word = {}	# word count of 2265 documant (word, number of words)
+background_model = {}	# word count of 2265 document (word, number of words)
 query = {}				# query
-my_lambda = 0.5
+query_lambda = 0.4
+doc_lambda = 0.8
 
-# list all files of a directory(Document)
-for dir_item in os.listdir(documant_path):
-	# join dir path and file name
-    dir_item_path = os.path.join(documant_path, dir_item)
-	# check whether a file exists before read
-    if os.path.isfile(dir_item_path):
-        with open(dir_item_path, 'r') as f:
-			# read content of document (doc, content)
-            data[dir_item] = f.read()
+document_path = "../Corpus/SPLIT_DOC_WDID_NEW"
+query_path = "../Corpus/QUERY_WDID_NEW_middle"
 
 # preprocess
-data = ProcDoc.doc_preprocess(data)
+data = ProcDoc.read_file(document_path)
+doc_wordcount = ProcDoc.doc_preprocess(data)
+doc_unigram = ProcDoc.unigram(doc_wordcount)
 
-
-# count background_word
+# count background_model
 '''
 for key, value in data.items():
-	background_word = ProcDoc.word_count(value, background_word)
-print len(background_word)
-background_word_sum = ProcDoc.word_sum(background_word)
+	background_model = ProcDoc.word_count(value, background_model)
+print len(background_model)
+background_word_sum = ProcDoc.word_sum(background_model)
 '''
-background_word = ProcDoc.read_background_dict()
-
-# 16 query documants
-for query_item in os.listdir(query_path):
-	# join dir path and file name
-    query_item_path = os.path.join(query_path, query_item)
-	# check whether a file exists before read
-    if os.path.isfile(query_item_path):
-        with open(query_item_path, 'r') as f:
-			# read content of query documant (doc, content)
-            query[query_item] = f.read()
+background_model = ProcDoc.read_background_dict()
 
 # preprocess
+query = ProcDoc.read_file(query_path)
 query = ProcDoc.query_preprocess(query)
-query_word_count = {}
+query_wordcount = {}
+
 for q, q_content in query.items():
-	query_word_count[q] = ProcDoc.word_count(q_content, {})
+	query_wordcount[q] = ProcDoc.word_count(q_content, {})
+
+query_unigram = ProcDoc.unigram(query_wordcount)
+query_model = ProcDoc.modeling(query_unigram, background_model, query_lambda)
 
 # query process
 assessment = readAssessment.get_assessment()
-lambda_test = {0: 0}
-interval	= 0.1
-isBreak = "run"
-while isBreak != "exit":
-	for my_lambda in np.arange(0, 1, interval):
-		if my_lambda in lambda_test: 	continue
-		else:							
-			docs_point = {}
-			AP = 0
-			mAP = 0
-			for q_key, q_val in query.items():
-				for doc_key, doc_words_prob in data.items():
-					point = 0
-					# calculate each query value for the document
-					for query_word in q_val.split():
-						count = 0						# C(w , D)
-						word_probability = 0			# P(w | D)
-						background_probability = 0		# BG(w | D)
-						# check if word at query exists in the document
-						if query_word in doc_words_prob:
-							word_probability = doc_words_prob[query_word]
-						background_probability = background_word[query_word]
-						
-						point += query_word_count[q_key][query_word]*log(my_lambda * word_probability + (1 - my_lambda ) * background_probability)
-					docs_point[doc_key] = point
-				# sorted each doc of query by point
-				docs_point_list = sorted(docs_point.items(), key=operator.itemgetter(1), reverse = True)
-				AP += readAssessment.precision(docs_point_list, assessment[q_key])
-			# mean average precision	
-			mAP = AP / len(query)
-			print my_lambda
-			print mAP
-			lambda_test[my_lambda] = mAP
-	# get key with maximum value in lambda_test dictionary	
-	max_lambda = max(lambda_test.iteritems(), key=operator.itemgetter(1))[0]
-	max_mAP = lambda_test[my_lambda]
-	# print Lambda and Max value
-	print "Max:"
-	print max_lambda, max_mAP
-	isBreak = raw_input("exit ?\n")
-	interval /= 10
+
+docs_point = {}
+AP = 0
+mAP = 0
+for q_key, q_word_prob in query_model.items():
+	for doc_key, doc_words_prob in doc_unigram.items():
+		point = 0
+		# calculate each query value for the document
+		for query_word, query_prob in q_word_prob.items():
+			count = 0						# C(w , D)
+			word_probability = 0			# P(w | D)
+			# check if word at query exists in the document
+			if query_word in doc_words_prob:
+				word_probability = doc_words_prob[query_word]
+			
+			# (query model) * log(doc_model) 			
+			point += query_model[q_key][query_word] * log((1-doc_lambda) * word_probability + doc_lambda * background_model[query_word])
+		docs_point[doc_key] = point
+		# sorted each doc of query by point
+		docs_point_list = sorted(docs_point.items(), key=operator.itemgetter(1), reverse = True)
+	AP += readAssessment.precision(docs_point_list, assessment[q_key])
+# mean average precision	
+mAP = AP / len(query)
+print mAP
