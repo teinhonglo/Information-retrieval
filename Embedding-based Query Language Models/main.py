2d0e4edb44a43ba5d639c4fe1d48b9453f90dabd
zweiein <evangelinathanasiakittymcdowel@gmail.com>
2017-05-10 22:07:25 +0800

    train word2vec 0.39

--- Embedding-based Query Language Models/main.py
+++ Embedding-based Query Language Models/main.py
@@ -61,14 +61,15 @@ for rm_file in remove_list:
 		os.remove("model/" + rm_file)
 		
 # Embedded Query Expansion
-m_list = np.linspace(20, 70, num=51)
+m_list = np.linspace(10, 80, num=71)
+m = 33
 interpolated_aplpha_list = np.linspace(0, 1.0, num=11)
 word2vec = word2vec_model.word2vec_model()
 
 EQE1 = []
 EQE2 = []
 for m in m_list:
-	[tmp_eqe1, tmp_eqe2] = Embedded_based.EmbeddedQuery(query_wordcount, collection, word2vec, 0.5, m)
+	[tmp_eqe1, tmp_eqe2] = Embedded_based.EmbeddedQuery(query_wordcount, collection, word2vec, 0.5, int(m))
 	EQE1.append(tmp_eqe1)
 	EQE2.append(tmp_eqe2)
 
@@ -117,5 +118,6 @@ for query_model in EQE2:
 			
 		#query_model = Expansion.feedback(query_docs_point_fb, query_model_fb, dict(doc_unigram), dict(doc_wordcount), dict(general_model), dict(background_model), step + 1)
 	
+print np.argmax(np.array(mAP_list), axis = 0), mAP_list[np.argmax(np.array(mAP_list), axis = 0)]
+plot_diagram.plotList(m_list, mAP_list, "Query-Independent Term Similarities", "mAP")
 
-plot_diagram.plotList(interpolated_aplpha_list, mAP_list, "Query-Independent Term Similarities", "mAP")
