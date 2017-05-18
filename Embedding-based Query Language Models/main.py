6f635e551c5a8fad2e8c5eca14c3fc87f0169191
teinhonglo <teinhonglo@gmail.com>
2017-05-19 02:33:46 +0800

    un query term

--- Embedding-based Query Language Models/main.py
+++ Embedding-based Query Language Models/main.py
@@ -17,7 +17,7 @@ general_model = {}
 query = {}				# query
 query_lambda = 0.4
 doc_lambda = 0.8
-remove_list = ["collection_embedded.pkl", "query_embedded.pkl", "update_embedded_query_expansion_ci.pkl", "update_embedded_query_expansion_qi.pkl", "collection_total_similarity.pkl"]
+remove_list = []#["update_embedded_query_expansion_ci.pkl", "update_embedded_query_expansion_qi.pkl", "collection_embedded.pkl", "query_embedded.pkl", "collection_total_similarity.pkl"]
 
 document_path = "../Corpus/SPLIT_DOC_WDID_NEW"
 query_path = "../Corpus/QUERY_WDID_NEW_middle"
@@ -62,21 +62,21 @@ for rm_file in remove_list:
 		
 # Embedded Query Expansion
 m_list = np.linspace(0, 80, num=81)
-m = 27
+m = 1
 interpolated_aplpha_list = np.linspace(0, 1.0, num=11)
 word2vec = word2vec_model.word2vec_model()
 
 EQE1 = []
 EQE2 = []
-for m in m_list:
-	[tmp_eqe1, tmp_eqe2] = Embedded_based.EmbeddedQuery(query_wordcount, collection, word2vec, 0.5, int(m))
+for interpolated_aplpha in interpolated_aplpha_list:
+	[tmp_eqe1, tmp_eqe2] = Embedded_based.EmbeddedQuery(query_wordcount, collection, word2vec, interpolated_aplpha, int(m))
 	tmp_eqe1 = ProcDoc.modeling(tmp_eqe1, background_model, query_lambda)
 	tmp_eqe2 = ProcDoc.modeling(tmp_eqe2, background_model, query_lambda)
 	EQE1.append(tmp_eqe1)
 	EQE2.append(tmp_eqe2)
 
-Pickle.dump(EQE1, open("model/eqe1_10_a50.pkl", "wb"), True)
-Pickle.dump(EQE2, open("model/eqe2_10_a50.pkl", "wb"), True)
+Pickle.dump(EQE1, open("model/eqe1_10.pkl", "wb"), True)
+Pickle.dump(EQE2, open("model/eqe2_10.pkl", "wb"), True)
 '''
 
 EQE1 = Pickle.load(open("model/eqe1_10.pkl", "rb"))
@@ -88,7 +88,7 @@ assessment = readAssessment.get_assessment()
 query_docs_point_fb = {}
 query_model_fb = {}
 mAP_list = []
-for query_model in EQE2:
+for query_model in EQE1:
 	for step in range(1):
 		query_docs_point_dict = {}
 		AP = 0
@@ -128,6 +128,6 @@ for query_model in EQE2:
 		'''
 	
 print np.argmax(np.array(mAP_list), axis = 0), mAP_list[np.argmax(np.array(mAP_list), axis = 0)]
-plot_diagram.plotList(interpolated_aplpha_list, mAP_list, "Query-Independent Term Similarities", "mAP")
+plot_diagram.plotList(interpolated_aplpha_list, mAP_list, "Conditional Independence of Query Terms", "mAP")
 
 
