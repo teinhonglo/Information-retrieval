9bfd97c13f48ca6b3bc536433b5683d4d307bd92
zweiein <evangelinathanasiakittymcdowel@gmail.com>
2017-06-02 12:46:14 +0800

    file io memory

--- Relevance-based Word Embedding/main.py
+++ Relevance-based Word Embedding/main.py
@@ -9,13 +9,15 @@ rel_qry_lambda = 0.1
 qry_lambda = 0.1
 doc_lambda = 0.8
 
-query_model = Pickle.load(open("query_model.pkl", "rb"))
-query_list = Pickle.load(open("query_list.pkl", "rb"))
+with open("query_model.pkl", "rb") as file: query_model = Pickle.load(file)
+with open("query_list.pkl", "rb") as file:	query_list = Pickle.load(file)
 print query_model.shape
 
-doc_model = Pickle.load(open("doc_model.pkl", "rb"))
-doc_list = Pickle.load(open("doc_list.pkl", "rb"))
+with open("doc_model.pkl", "rb") as file: doc_model = Pickle.load(file)
+with open("doc_list.pkl", "rb") as file: doc_list = Pickle.load(file)
 print doc_model.shape
+
+with open("relevance_model_RM.pkl", "rb") as file : rel_query_model = Pickle.load(file)
 
 background_model = ProcDoc.read_background_dict()
 print background_model.shape
@@ -28,43 +30,38 @@ for doc_idx in range(doc_model.shape[0]):
 	doc_model[doc_idx] = (1 - doc_lambda) * doc_vec + doc_lambda * background_model
 
 mAP_list = []
-total_mAP_list = []
 query_rel_list = []
 query_bg_list = []	
 doc_model = np.log(doc_model)
-for smoothing in np.linspace(0.1, 0.9, num = 9):
-	rel_query_model = Pickle.load(open("relevance_model_"+str(smoothing)+".pkl", "rb"))
-	for rel_qry_lambda in np.linspace(0, 1., num=11):
-		''' query smoothing '''	
-		query_model = Pickle.load(open("query_model.pkl", "rb"))
-		for qry_idx in range(query_model.shape[0]):
-			qry_vec = query_model[qry_idx]
-			query_model[qry_idx] = (1 - rel_qry_lambda) * qry_vec + rel_qry_lambda * rel_query_model[qry_idx]
-			#query_model[qry_idx] = (1 - qry_lambda) * qry_vec + qry_lambda * background_model
-		result = np.argsort(-np.dot(query_model, doc_model.T), axis = 1)
-		query_docs_ranking = {}
-		''' speedup '''
-		for q_idx in range(len(query_list)):
-			docs_ranking = []
-			for doc_idx in result[q_idx]:
-				docs_ranking.append(doc_list[doc_idx])
-			query_docs_ranking[query_list[q_idx]] = docs_ranking
+
+
+for rel_qry_lambda in np.linspace(0, 1., num=11):
+	''' query smoothing '''	
+	with open("query_model.pkl", "rb") as file: query_model = Pickle.load(file)
+	for qry_idx in range(query_model.shape[0]):
+		qry_vec = query_model[qry_idx]
+		query_model[qry_idx] = (1 - rel_qry_lambda) * qry_vec + rel_qry_lambda * rel_query_model[qry_idx]
+		#query_model[qry_idx] = (1 - qry_lambda) * qry_vec + qry_lambda * background_model
+	result = np.argsort(-np.dot(query_model, doc_model.T), axis = 1)
+	query_docs_ranking = {}
+	''' speedup '''
+	for q_idx in range(len(query_list)):
+		docs_ranking = []
+		for doc_idx in result[q_idx]:
+			docs_ranking.append(doc_list[doc_idx])
+		query_docs_ranking[query_list[q_idx]] = docs_ranking
+	
+	''' query 
+	for query_key, query_vec in  zip(query_list, query_model):
+		print len(query_docs_ranking.keys())
+		query_result = np.argsort(-(query_vec * doc_model).sum(axis = 1))
+		docs_ranking = []
+		for doc_idx in query_result:
+			docs_ranking.append(doc_list[doc_idx])
+			query_docs_ranking[query_key] = docs_ranking
 		
-		''' query 
-		for query_key, query_vec in  zip(query_list, query_model):
-			print len(query_docs_ranking.keys())
-			query_result = np.argsort(-(query_vec * doc_model).sum(axis = 1))
-			docs_ranking = []
-			for doc_idx in query_result:
-				docs_ranking.append(doc_list[doc_idx])
-				query_docs_ranking[query_key] = docs_ranking
-			
-		mAP = eval.mean_average_precision(query_docs_ranking)	
-		print mAP, qry_lambda, rel_qry_lambda
-		'''
-		mAP = eval.mean_average_precision(query_docs_ranking)	
-		mAP_list.append(mAP)
-		print mAP, smoothing, rel_qry_lambda
-	total_mAP_list.append(max(mAP_list))
-	print 
-print max(total_mAP_list), np.argmax(np.array(total_mAP_list))	
+	mAP = eval.mean_average_precision(query_docs_ranking)	
+	print mAP, qry_lambda, rel_qry_lambda
+	'''
+	mAP = eval.mean_average_precision(query_docs_ranking)	
+	print mAP
--- Relevance-based Word Embedding/relevance_model.py
+++ Relevance-based Word Embedding/relevance_model.py
@@ -15,48 +15,48 @@ from math import exp
 
 topM = 10
 vocabulary_size = 51253
-for smoothing in np.linspace(0.1, 0.9, num=9):
+smoothing = 0.1
 
-	''' load data'''
-	with open("query_list.pkl", "rb") as file:
-		query_list = pickle.load(file)
-	with open("query_model.pkl", "rb") as file:	
-		query_model = pickle.load(file)
-	with open("doc_list.pkl", "rb") as file:
-		doc_list = pickle.load(file)
-	with open("doc_model.pkl", "rb") as file:	
-		doc_model = pickle.load(file)
-	with open("HMMTraingSetDict.pkl", "rb") as file:
-		query_docs_ranking = pickle.load(file)
+''' load data'''
+with open("query_list.pkl", "rb") as file:
+	query_list = pickle.load(file)
+with open("query_model.pkl", "rb") as file:	
+	query_model = pickle.load(file)
+with open("doc_list.pkl", "rb") as file:
+	doc_list = pickle.load(file)
+with open("doc_model.pkl", "rb") as file:	
+	doc_model = pickle.load(file)
+with open("HMMTraingSetDict.pkl", "rb") as file:
+	query_docs_ranking = pickle.load(file)
 
-	background_model =  ProcDoc.read_background_dict()	
+background_model =  ProcDoc.read_background_dict()	
 
-	''' smoothing '''
-	for d_idx, doc_vec in enumerate(doc_model):
-		doc_model[d_idx] = (1 - smoothing) * doc_vec + smoothing * background_model
+''' smoothing '''
+for d_idx, doc_vec in enumerate(doc_model):
+	doc_model[d_idx] = (1 - smoothing) * doc_vec + smoothing * background_model
 
-	''' relevace model '''
-	for q_idx, q_key in enumerate(query_list):
-		q_vec = query_model[q_idx]
-		# relevance top M document
-		q_t_d = np.zeros(len(query_docs_ranking[q_key]))
-		w_d = np.zeros(vocabulary_size)
-		for rank_idx, doc_key in enumerate(query_docs_ranking[q_key]):
-			doc_idx = doc_list.index(doc_key)
-			doc_vec = doc_model[doc_idx]
-			# probability of query term in document
-			q_non_zero, = np.where(q_vec != 0)
-			# product
-			# q_t_d[rank_idx] = (np.prod(doc_vec[q_non_zero]) + 0.1)
-			# logadd
-			for q_t in np.log(doc_vec[q_non_zero]):
-				q_t_d[rank_idx] += q_t
-			#print exp(q_t_d[rank_idx])
-			
-			w_d += doc_vec * q_t_d[rank_idx]
-		# relevance model
-		w_d /= q_t_d.sum(axis = 0)
-		query_model[q_idx] = w_d
+''' relevace model '''
+for q_idx, q_key in enumerate(query_list):
+	q_vec = query_model[q_idx]
+	# relevance top M document
+	q_t_d = np.zeros(len(query_docs_ranking[q_key]))
+	w_d = np.zeros(vocabulary_size)
+	for rank_idx, doc_key in enumerate(query_docs_ranking[q_key]):
+		doc_idx = doc_list.index(doc_key)
+		doc_vec = doc_model[doc_idx]
+		# probability of query term in document
+		q_non_zero, = np.where(q_vec != 0)
+		# product
+		# q_t_d[rank_idx] = (np.prod(doc_vec[q_non_zero]) + 0.1)
+		# logadd
+		for q_t in np.log(doc_vec[q_non_zero]):
+			q_t_d[rank_idx] += q_t
+		#print exp(q_t_d[rank_idx])
 		
-	with open("relevance_model_"+str(smoothing)+".pkl", "wb") as file:	
-		pickle.dump(query_model, file, True)
+		w_d += doc_vec * q_t_d[rank_idx]
+	# relevance model
+	w_d /= q_t_d.sum(axis = 0)
+	query_model[q_idx] = w_d
+	
+with open("relevance_model_RM.pkl", "wb") as file:	
+	pickle.dump(query_model, file, True)
