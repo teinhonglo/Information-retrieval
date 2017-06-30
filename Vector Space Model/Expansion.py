6a2f509367a0d621fbe175dbfd589b4d75ef1051
zweiein <evangelinathanasiakittymcdowel@gmail.com>
2017-06-30 19:46:44 +0800

    vsm spoken doc

--- Vector Space Model/Expansion.py
+++ Vector Space Model/Expansion.py
@@ -6,6 +6,7 @@ import fileinput
 import collections
 from math import exp
 import codecs
+import copy
 
 def extQueryModel(query_model, rank_list, doc_model, feedback_model, top_N):
 	'''
@@ -13,14 +14,17 @@ def extQueryModel(query_model, rank_list, doc_model, feedback_model, top_N):
 	rank_list: {doc_name, doc_rank_value}
 	doc_model: {doc_name, doc_word_count}
 	'''
+	
 	for query, query_wc in query_model.items():
-		for doc_name, doc_rank_value in rank_list[query][0:top_N]:
+		ex_query_wc = copy.deepcopy(query_wc)
+		for doc_name in rank_list[query][0:top_N]:
 			if not doc_name in feedback_model:
 				feedback_model.append(doc_name)
 				for word, count in doc_model[doc_name].items():
 					if word in query_model[query]:
-						query_model[query][word] = query_model[query][word] + count
+						ex_query_wc[word] = query_model[query][word] + count
 					else:
-						query_model[query][word] = count
+						ex_query_wc[word] = count
+		query_model[query] = ex_query_wc
 	return [query_model, feedback_model]
 
