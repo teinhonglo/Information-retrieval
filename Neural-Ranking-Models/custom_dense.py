a75b2ae19070eff600d2caf0de38a2daf6c03844
zweiein <evangelinathanasiakittymcdowel@gmail.com>
2017-08-08 18:25:07 +0800

    NRM attempt

---
+++ Neural-Ranking-Models/custom_dense.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+
+import numpy as np
+
+import copy
+import types as python_types
+import warnings
+
+from keras import backend as K
+from keras import activations
+from keras import initializations
+from keras import regularizers
+from keras import constraints
+from keras.engine import InputSpec
+from keras.engine import Layer
+from keras.utils.generic_utils import func_dump
+from keras.utils.generic_utils import func_load
+from keras.utils.generic_utils import deserialize_keras_object
+from keras.utils.generic_utils import has_arg
+from keras.legacy import interfaces
+
+
+class Dense(Layer):
+    """Just your regular densely-connected NN layer.
+    `Dense` implements the operation:
+    `output = activation(dot(input, kernel) + bias)`
+    where `activation` is the element-wise activation function
+    passed as the `activation` argument, `kernel` is a weights matrix
+    created by the layer, and `bias` is a bias vector created by the layer
+    (only applicable if `use_bias` is `True`).
+    Note: if the input to the layer has a rank greater than 2, then
+    it is flattened prior to the initial dot product with `kernel`.
+    # Example
+    ```python
+        # as first layer in a sequential model:
+        model = Sequential()
+        model.add(Dense(32, input_shape=(16,)))
+        # now the model will take as input arrays of shape (*, 16)
+        # and output arrays of shape (*, 32)
+        # after the first layer, you don't need to specify
+        # the size of the input anymore:
+        model.add(Dense(32))
+    ```
+    # Arguments
+        units: Positive integer, dimensionality of the output space.
+        activation: Activation function to use
+            (see [activations](../activations.md)).
+            If you don't specify anything, no activation is applied
+            (ie. "linear" activation: `a(x) = x`).
+        use_bias: Boolean, whether the layer uses a bias vector.
+        kernel_initializer: Initializer for the `kernel` weights matrix
+            (see [initializers](../initializers.md)).
+        bias_initializer: Initializer for the bias vector
+            (see [initializers](../initializers.md)).
+        kernel_regularizer: Regularizer function applied to
+            the `kernel` weights matrix
+            (see [regularizer](../regularizers.md)).
+        bias_regularizer: Regularizer function applied to the bias vector
+            (see [regularizer](../regularizers.md)).
+        activity_regularizer: Regularizer function applied to
+            the output of the layer (its "activation").
+            (see [regularizer](../regularizers.md)).
+        kernel_constraint: Constraint function applied to
+            the `kernel` weights matrix
+            (see [constraints](../constraints.md)).
+        bias_constraint: Constraint function applied to the bias vector
+            (see [constraints](../constraints.md)).
+    # Input shape
+        nD tensor with shape: `(batch_size, ..., input_dim)`.
+        The most common situation would be
+        a 2D input with shape `(batch_size, input_dim)`.
+    # Output shape
+        nD tensor with shape: `(batch_size, ..., units)`.
+        For instance, for a 2D input with shape `(batch_size, input_dim)`,
+        the output would have shape `(batch_size, units)`.
+    """
+
+    @interfaces.legacy_dense_support
+    def __init__(self, units,
+                 activation=None,
+                 use_bias=True,
+                 kernel_initializer='glorot_uniform',
+                 bias_initializer='zeros',
+                 kernel_regularizer=None,
+                 bias_regularizer=None,
+                 activity_regularizer=None,
+                 kernel_constraint=None,
+                 bias_constraint=None,
+                 **kwargs):
+        if 'input_shape' not in kwargs and 'input_dim' in kwargs:
+            kwargs['input_shape'] = (kwargs.pop('input_dim'),)
+        super(Dense, self).__init__(**kwargs)
+        self.units = units
+        self.activation = activations.get(activation)
+        self.use_bias = use_bias
+        self.kernel_initializer = initializers.get(kernel_initializer)
+        self.bias_initializer = initializers.get(bias_initializer)
+        self.kernel_regularizer = regularizers.get(kernel_regularizer)
+        self.bias_regularizer = regularizers.get(bias_regularizer)
+        self.activity_regularizer = regularizers.get(activity_regularizer)
+        self.kernel_constraint = constraints.get(kernel_constraint)
+        self.bias_constraint = constraints.get(bias_constraint)
+        self.input_spec = InputSpec(min_ndim=2)
+        self.supports_masking = True
+
+if __name__ == "__main__":
+	d = Dense()
---
+++ Neural-Ranking-Models/rank_func_c.py
@@ -0,0 +1,116 @@
+import numpy as np
+
+np.random.seed(1337)
+
+from keras.models import Sequential, Model
+import theano
+from keras.engine.topology import Layer
+from keras.layers import Dense, Dropout, Input, Lambda, merge, Embedding, LSTM
+from keras.layers.core import Reshape
+from keras.layers.wrappers import TimeDistributed
+from keras import backend as K
+from keras.engine.topology import Layer
+
+''' weight softmax function '''
+def softmax_reg(weight_matrix):
+    ndim = K.ndim(weight_matrix)
+    if ndim == 2:
+        return K.softmax(weight_matrix)
+    elif ndim > 2:
+        e = K.exp(weight_matrix - K.max(weight_matrix, axis=axis, keepdims=True))
+        s = K.sum(e, axis=axis, keepdims=True)
+        return e / s
+    else:
+        raise ValueError('Cannot apply softmax to a tensor that is 1D')
+
+''' custom loss function'''
+def hinge_loss(y_true, y_pred):
+    epsilon = 1.
+    return K.mean(K.maximum(0, epsilon-(K.sign(y_true) * y_pred)), axis=-1 )
+
+''' calculate the dist '''
+def relative_distance(vects):
+    x, y = vects
+    print x, y
+    return x - y
+
+def dist_output_shape(shapes):
+    shape1, shape2 = shapes
+    return (shape1[0], 1)
+
+# Network Parameter
+batch_size = 16    
+epochs = 50
+validation_split = 0.1
+vocab_size = 51253
+embed_dim = 64
+input_dim = 50
+
+# Number of input(query and document)
+qry_vec = Input(shape=(input_dim,), name = "qry_input") 
+doc_vec = Input(shape=(input_dim,), name = "doc_input") 
+
+# Embeddings Layer
+emb = Embedding(vocab_size, embed_dim, input_length = input_dim, name="emb_layer")
+# seq_layer = TimeDistributed(Dense(output_shape(None), kernel_regularizer=softmax_reg), input_shape=(input_dim, embed_dim), name="seq_layer")
+seq_layer = Lambda(lambda x: K.sum(x, axis=1), output_shape=lambda s: (s[0], s[2]), name="seq_layer")
+# emb_rsp = Reshape((input_dim, embed_dim), name = "emb_reshape")
+# seq_layer = Dense(1)
+
+# Base Network
+h1 = Dense(128, input_shape=(input_dim,), name = "h1")
+h1_dp = Dropout(0.2)
+h2 = Dense(256, activation='sigmoid', name = "h2")
+h2_dp = Dropout(0.2)
+h3 = Dense(128, activation='sigmoid', name = "h3")
+h3_dp = Dropout(0.2)
+h4_rk = Dense(1, name = "h4_score")
+
+# Embedding and Pojection
+qry_rep = seq_layer(emb(qry_vec))
+doc_rep = seq_layer(emb(doc_vec))
+
+qry_h1 = h1_dp(h1(qry_rep))
+doc_h1 = h1_dp(h1(doc_rep))
+
+qry_h2 = h2_dp(h2(qry_h1))
+doc_h2 = h2_dp(h2(doc_h1))
+
+qry_h3 = h3_dp(h3(qry_h2))
+doc_h3 = h3_dp(h3(doc_h2))
+
+encoded_qry = h4_rk(qry_h3)
+encoded_doc = h4_rk(doc_h3)
+
+# Concatenate the two vectors
+predicts = Lambda(relative_distance,
+                  output_shape=dist_output_shape)([encoded_qry, encoded_doc])
+                  
+# Define a trainable model linking the the predicts
+model = Model(inputs=[qry_vec, doc_vec], outputs=predicts)
+
+model.compile(optimizer='Adam',
+              loss=hinge_loss,
+              metrics=['accuracy'])
+
+model.summary()
+
+# preprocess data
+data_a = np.random.rand(100, input_dim) * 3
+data_b = np.random.rand(100, input_dim) * (-3)
+
+labels = np.sum(data_a - data_b, axis = 1)
+labels = labels.astype(np.float32)
+
+from keras.utils import plot_model
+plot_model(model, to_file='model.png')
+
+history_adam = model.fit([data_a, data_b], labels,
+                        batch_size=batch_size,
+                        epochs=epochs,
+                        verbose=1,
+                        shuffle=True,
+                        validation_split=validation_split)
+                        
+#model.save('rank_function.h5')                                
+
