5cf26a57f052817d5eeb626287626b553dd2a82c
zweiein <evangelinathanasiakittymcdowel@gmail.com>
2017-08-30 14:54:09 +0800

    re-structure for storage path

--- RMWE-cluster/clustering.py
+++ RMWE-cluster/clustering.py
@@ -6,7 +6,8 @@ import cPickle as Pickle
 import kmeans
 
 # Read Query Model
-model_path = "../Corpus/model/TDT2/UM/"
+corpus = "TDT2"
+model_path = "../Corpus/model/"+corpus+"/UM/"
 query_model = Pickle.load(model_path + "query_model.pkl")
 # Preprocess(fit data structure in kmeans.py)
 for idx, vec in enumerate(query_model):
@@ -19,27 +20,16 @@ for num_of_cluster in [2, 4, 8]:
 	# return clusters and centeroids
 	[clusters, centroids] = kmeans.kmeans(data_list, num_of_cluster)	
 
-	with open('clusters/kmeans_' + str(num_of_cluster) + '.txt', 'w') as output:
+	with open("clusters/"+corpus+"/kmeans_" + str(num_of_cluster) + '.txt', 'w') as output:
 		# Compute SSE
 		sse = 0
 		for cur_cluster in xrange(0, len(clusters)):
 			# Cluster 
 			cluster_name = str(cur_cluster) + "," + centroids[cur_cluster]
 			output.write(cluster_name)
-			
-			# Centeroid
-			#print centroids[cur_cluster]
-			#cluster_data = []
-			data_str = ""
-			
 			# Item
+			data_str = ""
 			for data in clusters[cur_cluster]:
 				data_str += "," + data.getID()
-				#cluster_data.append(data.getCoor())
 			output.write(data_str)
 			output.write("\n")
-			
-#			# Sum of Squared Error
-#			sse += ((cluster_data - centroids[cur_cluster]) ** 2).sum(axis = 1).sum(axis = 0)
-#		output.write(unicode("SSE : ", 'utf-8', errors="replace"))
-#		output.write(unicode(str(sse) + "\n", 'utf-8', errors="replace"))
