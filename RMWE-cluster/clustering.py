ccbcf281540280981a3f294e56f204cf999366f2
zweiein <evangelinathanasiakittymcdowel@gmail.com>
2017-08-30 17:52:38 +0800

    centroids

--- RMWE-cluster/clustering.py
+++ RMWE-cluster/clustering.py
@@ -8,7 +8,7 @@ import kmeans
 # Read Query Model
 corpus = "TDT2"
 model_path = "../Corpus/model/"+corpus+"/UM/"
-query_model = Pickle.load(model_path + "query_model.pkl")
+with open(model_path + "query_model.pkl", "rb") as f: query_model = Pickle.load(f)
 # Preprocess(fit data structure in kmeans.py)
 for idx, vec in enumerate(query_model):
 	data = kmeans.dataInfo(idx, vec)
@@ -25,7 +25,7 @@ for num_of_cluster in [2, 4, 8]:
 		sse = 0
 		for cur_cluster in xrange(len(clusters)):
 			# Cluster 
-			cluster_name = str(cur_cluster) + "," + centroids[cur_cluster]
+			cluster_name = str(cur_cluster)
 			output.write(cluster_name)
 			# Item
 			data_str = ""
@@ -33,3 +33,4 @@ for num_of_cluster in [2, 4, 8]:
 				data_str += "," + data.getID()
 			output.write(data_str)
 			output.write("\n")
+	with open("clusters/"+corpus+"/kmeans_centroids_" + str(num_of_cluster) + '.pkl', 'wb') as f: Pickle.dump(centeroids, f, True)
