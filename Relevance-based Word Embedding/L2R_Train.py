19a5292da8eb17bca1adf018d70a0786ed66ba99
zweiein <evangelinathanasiakittymcdowel@gmail.com>
2017-10-11 19:02:48 +0800

    L2R batch_dot

--- Relevance-based Word Embedding/L2R_Train.py
+++ Relevance-based Word Embedding/L2R_Train.py
@@ -10,23 +10,23 @@ import cPickle as pickle
 ''' Import keras to build a DL model '''
 from keras.layers import Dense, Dropout, Input, Lambda, merge, Embedding, LSTM, Convolution1D
 from keras.layers.core import Reshape, Dense, Activation, Dropout
+from keras.layers.merge import concatenate, dot
 from keras.models import Sequential, Model
 from keras import backend as K
 
 model_path = "../Corpus/model/TDT2/UM/"
-optimizer = ["Adagrad" , "Adam", "Nadam"]
-losses = ["categorical_crossentropy", "kullback_leibler_divergence"]
 
 ''' custom loss function'''
 def hinge_loss(y_true, y_pred):
     epsilon = 1.
     return K.mean(K.maximum(0, epsilon-(K.sign(y_true) * y_pred)), axis=-1 )
 
-def kl_distance(vects):
+def kl_dist(vects):
     qry_vec, doc_vec = vects
     qry_vec = K.clip(qry_vec, K.epsilon(), 1)
     doc_vec = K.clip(doc_vec, K.epsilon(), 1)
-    return K.sum(qry_vec * K.log(doc_vec), axis=-1)
+    dist = K.batch_dot(qry_vec, K.log(doc_vec), 1)
+    return dist
 
 def relative_distance(vects):
     x, y = vects
@@ -42,8 +42,9 @@ def rel_shape(shapes):
 
 with open(model_path + "query_model.pkl", "rb") as file: query_model = pickle.load(file)
 X_train = query_model
+print X_train.shape
 with open("obj_func/TDT2/rel_supervised_swlm_entropy.pkl", "rb") as file: query_relevance = pickle.load(file)
-Y_train = np.random.rand(800, 1)#np.sum(query_relevance, axis = 1)
+Y_train = np.zeros((800, ))
 print Y_train.shape
 #print Y_train.shape
 
@@ -59,30 +60,33 @@ print 'Building a model whose optimizer=adam, activation function=softmax'
 emb_layer = Dense(embedding_dimensionality, activation="linear", name="emb_layer")
 rep_layer = Dense(vocabulary_size, activation="softmax", name="rep_layer")
 
+
 qry = Input(shape=(vocabulary_size,), name="qry_input")
-doc_plus = Input(shape=(vocabulary_size,), name="doc_plus_input")
-doc_minus = Input(shape=(vocabulary_size,), name="doc_minus_input")
+pos_doc = Input(shape=(vocabulary_size,), name="pos_doc_input")
+neg_doc = Input(shape=(vocabulary_size,), name="neg_doc_input")
 
 qry_emb = emb_layer(qry)
-doc_emb_plus = emb_layer(doc_plus)
-doc_emb_minus = emb_layer(doc_minus)
+pos_doc_emb = emb_layer(pos_doc)
+neg_doc_emb = emb_layer(neg_doc)
 
 qry_rep = rep_layer(qry_emb)
-doc_rep_plus = rep_layer(doc_emb_plus)
-doc_rep_minus = rep_layer(doc_emb_minus)
+pos_doc_rep = rep_layer(pos_doc_emb)
+neg_doc_rep = rep_layer(neg_doc_emb)
 
-plus_score = Lambda(kl_distance, output_shape=kl_shape, name="qry_plus")([qry_rep, doc_rep_plus])
-minus_score = Lambda(kl_distance, output_shape=kl_shape, name="qry_minus")([qry_rep, doc_rep_minus])
+pos_score = Lambda(kl_dist, output_shape=kl_shape, name="pos_kl")([qry_rep, pos_doc_rep])
+neg_score = Lambda(kl_dist, output_shape=kl_shape, name="neg_kl")([qry_rep, neg_doc_rep])
+'''
+pos_score = dot([qry_rep, pos_doc_rep], axes = 1, normalize = True, name="pos_cos")
+neg_score = dot([qry_rep, neg_doc_rep], axes = 1, normalize = True, name="neg_cos")
+'''
+predicts = Lambda(relative_distance, output_shape=rel_shape)([pos_score, neg_score])
 
-predicts = Lambda(relative_distance, output_shape=rel_shape)([plus_score, minus_score])
-
-model = Model(inputs=[qry, doc_plus, doc_minus], outputs=predicts)
+model = Model(inputs=[qry, pos_doc, neg_doc], outputs=predicts)
 model.summary()
 
 ''' Setting optimizer as Adam '''
-from keras.optimizers import Adam, SGD
-model.compile(loss= hinge_loss,
-              optimizer='Adam')
+from keras.optimizers import Adam
+model.compile(loss= hinge_loss,optimizer='Adam')
 
 from keras.utils import plot_model
 plot_model(model, to_file='model.png')
@@ -94,10 +98,7 @@ history_adam = model.fit([X_train, X_train, X_train], Y_train,
 			verbose=1,
 			shuffle=True,
 			validation_split=0
-			#callbacks = [modelCheckpoint]
-			# earlyStopping callbacks
-			#callbacks = [earlyStopping]
 			)
 
 ''' Create a HDF5 file '''							
-#model.save('NN_Model/TDT2/RLE_SSWLM_E.h5')
+model.save('NN_Model/TDT2/RLE_L2R_KL.h5')
