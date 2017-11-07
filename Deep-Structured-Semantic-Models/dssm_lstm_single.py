946439d67dbe641f3e3b220fd30cc793ccb4339b
teinhonglo <teinhonglo@gmail.com>
2017-11-08 00:48:03 +0800

    cnn & lstm

--- Deep-Structured-Semantic-Models/dssm_lstm_single.py
+++ Deep-Structured-Semantic-Models/dssm_lstm_single.py
@@ -7,13 +7,13 @@
 import numpy as np
 
 from keras import backend
-from keras.layers import Activation, Input
+from keras.layers import Activation, Input, LSTM, Masking
 from keras.layers.core import Dense, Lambda, Reshape
 from keras.layers.convolutional import Convolution1D
 from keras.layers.merge import concatenate, dot
 from keras.models import Model
 
-def create_model(J=3, WORD_DEPTH=300, K=300, L=128):
+def create_model(J=3, WORD_DEPTH=100, K=300, L=128):
     #LETTER_GRAM_SIZE = 3 # See section 3.2.
     #WINDOW_SIZE = 3 # See section 3.2.
     #TOTAL_LETTER_GRAMS = int(3 * 1e4) # Determined from data. See section 3.2.
@@ -27,38 +27,35 @@ def create_model(J=3, WORD_DEPTH=300, K=300, L=128):
     neg_docs = [Input(shape = (None, WORD_DEPTH)) for j in range(J)]
     # Input tensors holding the query, positive (clicked) document, and negative (unclicked) documents.
     # The first dimension is None because the queries and documents can vary in length.
-    query_conv = Convolution1D(K, FILTER_LENGTH, padding = "same", input_shape = (None, WORD_DEPTH), activation = "tanh")(query)
-    # Next, we apply a max-pooling layer to the convolved query matrix. 
-    query_max = Lambda(lambda x: backend.max(x, axis = 1), output_shape = (K, ))(query_conv) # See section 3.4.
-    query_proj = Dense(K, name="q_proj_1", activation="tanh")(query_max) # See section 3.4.
-    query_proj_2 = Dense(K, name="q_proj_2", activation="tanh")(query_proj) # See section 3.4.
-
-    # In this step, we generate the semantic vector represenation of the query. This
-    # is a standard neural network dense layer, i.e., y = tanh(W_s v + b_s). Again,
-    # the paper does not include bias units.
-    query_sem = Dense(L, name="q_sem", activation = "tanh")(query_proj_2) # See section 3.5.
 
     # The document equivalent of the above query model.
-    doc_conv = Convolution1D(K, FILTER_LENGTH, padding = "same", input_shape = (None, WORD_DEPTH), activation = "tanh")
-    doc_max = Lambda(lambda x: backend.max(x, axis = 1), output_shape = (K, ))
-    doc_proj = Dense(K, name="d_proj_1", activation="tanh")
-    doc_proj_2 = Dense(K, name="d_proj_2", activation="tanh")
-    doc_sem = Dense(L, name="d_sem", activation = "tanh")
+    qd_lstm = LSTM(K, input_shape=(None, WORD_DEPTH), name="LSTM")
+    qd_output = Dense(K, name="l_output", activation="tanh")
+    qd_proj = Dense(K, name="d_proj_1", activation="tanh")
+    qd_proj_2 = Dense(K, name="d_proj_2", activation="tanh")
+    qd_sem = Dense(L, name="d_sem", activation = "tanh")
         
-    pos_doc_conv = doc_conv(pos_doc)
-    neg_doc_convs = [doc_conv(neg_doc) for neg_doc in neg_docs]        
+    query_conv = qd_lstm(Masking(mask_value=.0)(query))
+    # Next, we apply a max-pooling layer to the convolved query matrix. 
+    query_max = qd_output(query_conv) # See section 3.4.
+    query_proj = qd_proj(query_max) # See section 3.4.
+    query_proj_2 = qd_proj_2(query_proj) # See section 3.4.
+    query_sem = qd_sem(query_proj_2) # See section 3.5.
+
+    pos_doc_conv = qd_lstm(Masking(mask_value=.0)(pos_doc))
+    neg_doc_convs = [qd_lstm(Masking(mask_value=.0)(neg_doc)) for neg_doc in neg_docs]        
     
-    pos_doc_max = doc_max(pos_doc_conv)
-    neg_doc_maxes = [doc_max(neg_doc_conv) for neg_doc_conv in neg_doc_convs]
+    pos_doc_max = qd_output(pos_doc_conv)
+    neg_doc_maxes = [qd_output(neg_doc_conv) for neg_doc_conv in neg_doc_convs]
 
-    pos_doc_proj = doc_proj(pos_doc_max)
-    neg_doc_projs = [doc_proj(neg_doc_max) for neg_doc_max in neg_doc_maxes]
+    pos_doc_proj = qd_proj(pos_doc_max)
+    neg_doc_projs = [qd_proj(neg_doc_max) for neg_doc_max in neg_doc_maxes]
 
-    pos_doc_proj2 = doc_proj_2(pos_doc_proj)
-    neg_doc_proj2s = [doc_proj_2(neg_doc_proj) for neg_doc_proj in neg_doc_projs]
+    pos_doc_proj2 = qd_proj_2(pos_doc_proj)
+    neg_doc_proj2s = [qd_proj_2(neg_doc_proj) for neg_doc_proj in neg_doc_projs]
 
-    pos_doc_sem = doc_sem(pos_doc_proj2)
-    neg_doc_sems = [doc_sem(neg_doc_proj2) for neg_doc_proj2 in neg_doc_proj2s]
+    pos_doc_sem = qd_sem(pos_doc_proj2)
+    neg_doc_sems = [qd_sem(neg_doc_proj2) for neg_doc_proj2 in neg_doc_proj2s]
 
     # This layer calculates the cosine similarity between the semantic representations of
     # a query and a document.
