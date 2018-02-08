6e6afd918e0ff680bd33736948ddb6b7a59836ca
teinhonglo <teinhonglo@gmail.com>
2018-02-09 06:02:06 +0800

    rename some variable

--- Neural-Passage-Model/Preprocess.py
+++ Neural-Passage-Model/Preprocess.py
@@ -5,48 +5,49 @@ import numpy as np
 import ProcDoc
 
 class InputDataProcess(object):
-	def __init__(self, num_of_heter_feats = 10, max_qry_length = 1794, max_doc_length = 2907, query_path = None, document_path = None, corpus = "TDT2"):
-		resPos = True
+	def __init__(self, num_of_homo_feats = 10, max_qry_length = 1794, max_doc_length = 2907, query_path = None, document_path = None, corpus = "TDT2"):
+		res_pos = True
+		str2int = True
 		self.max_qry_length = max_qry_length
 		self.max_doc_length = max_doc_length
-		self.num_of_heter_feats = num_of_heter_feats
+		self.num_of_homo_feats = num_of_homo_feats
 		if query_path == None: 
 			query_path = "../Corpus/" + corpus + "/Train/XinTrainQryTDT2/QUERY_WDID_NEW"
 		if document_path == None:
 			document_path = "../Corpus/" + corpus + "/SPLIT_DOC_WDID_NEW"
 		# read document, reserve position
 		doc = ProcDoc.read_file(document_path)
-		self.doc = ProcDoc.doc_preprocess(doc, resPos)
-
+		self.doc = ProcDoc.doc_preprocess(doc, res_pos, str2int)
+			
 		# read query, reserve position
 		qry = ProcDoc.read_file(query_path)
-		self.qry = ProcDoc.query_preprocess(qry, resPos)	
+		self.qry = ProcDoc.query_preprocess(qry, res_pos, str2int)	
 		
 		# HMMTrainingSet
 		self.hmm_training_set = ProcDoc.read_relevance_dict()
-		self.heter_feature = self.__genFeature(num_of_heter_feats)
+		self.homo_feats = self.__genFeature(num_of_homo_feats)
 		
 	def genPassageAndLabels(self, list_IDs, labels, batch_size):
 		qry = self.qry
 		doc = self.doc
 		max_qry_length = self.max_qry_length
 		max_doc_length = self.max_doc_length
-		heter_feature = self.heter_feature
-		num_of_heter_feats = self.num_of_heter_feats
+		homo_feats = self.homo_feats
+		num_of_homo_feats = self.num_of_homo_feats
 		psg_mat_batch = np.zeros((batch_size, max_qry_length, max_doc_length, 1))
-		heter_feature_batch = np.zeros((batch_size, num_of_heter_feats))
+		homo_feats_batch = np.zeros((batch_size, num_of_homo_feats))
 		rel_batch = np.zeros((batch_size))
 		# generate passage
 		for idx, data_ID in enumerate(list_IDs):
 			[q_id, d_id] = data_ID.split("_")
-			q_terms = map(int, qry[q_id].split())
-			d_terms = np.asarray(map(int, doc[d_id].split()))
+			q_terms = qry[q_id]
+			d_terms = np.asarray(doc[d_id])
 			psg_mat = np.asarray([[(q_t == d_terms)] for q_t in q_terms]).reshape(len(q_terms), len(d_terms), 1)
 			psg_mat = self.__mergeMat(np.zeros((max_qry_length, max_doc_length, 1)), psg_mat)
 			psg_mat_batch[idx] = np.copy(psg_mat)
-			heter_feature_batch[idx] = heter_feature[q_id]
+			homo_feats_batch[idx] = homo_feats[q_id]
 			rel_batch[idx] = labels[data_ID]
-		return [np.asarray(psg_mat_batch), np.asarray(heter_feature_batch), np.asarray(rel_batch)]
+		return [psg_mat_batch, homo_feats_batch, rel_batch]
 	
 	def genTrainValidSet(self):
 		print "generate training set and validation set"
@@ -75,15 +76,14 @@ class InputDataProcess(object):
 		partition['validation'] = [id for id in ID_list[num_of_train:]]
 		return [partition, labels]
 	
-	def __genFeature(self, num_of_heter_feats):
+	def __genFeature(self, num_of_homo_feats):
 		###################### TODO ######################
 		print "generate h features"
 		qry = self.qry
 		doc = self.doc
-		heter_feats = {}
-		for q_id in qry.keys():
-			heter_feats[q_id] = np.random.rand(num_of_heter_feats)
-		return heter_feats
+		homo_feats = np.zeros((len(qry.keys()), num_of_homo_feats))
+		
+		return homo_feats
 		
 	def __mergeMat(self, b1, b2, pos = [0, 0]):
 		[pos_v, pos_h] = pos
