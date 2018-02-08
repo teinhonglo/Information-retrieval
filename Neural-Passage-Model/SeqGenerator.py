a544dd835c65bd434846560b5b3fae89bde8279a
teinhonglo <teinhonglo@gmail.com>
2018-02-08 15:43:08 +0800

    can running

--- Neural-Passage-Model/SeqGenerator.py
+++ Neural-Passage-Model/SeqGenerator.py
@@ -1,62 +1,62 @@
 import numpy as np
 
 class DataGenerator(object):
-  'Generates data for Keras'
-  def __init__(self, dim_x = 32, dim_y = 32, dim_x1 = 32, batch_size = 32, shuffle = True):
-      'Initialization'
-      self.dim_x = dim_x
-	  self.dim_x1 = dim_x1
-      self.dim_y = dim_y
-      self.batch_size = batch_size
-      self.shuffle = shuffle
+	#'Generates data for Keras'
+	def __init__(self,input_data_process, dim_x = 32, dim_y = 32, dim_x1 = 32, batch_size = 32, shuffle = True):
+		#'Initialization'
+		self.input_data_process = input_data_process
+		self.dim_x = dim_x
+		self.dim_y = dim_y
+		self.dim_x1 = dim_x1
+		self.batch_size = batch_size
+		self.shuffle = shuffle
 
-  def generate(self, labels, list_IDs):
-      'Generates batches of samples'
-      # Infinite loop
-      while 1:
-          # Generate order of exploration of dataset
-          indexes = self.__get_exploration_order(list_IDs)
+	def generate(self, labels, list_IDs):
+		# Generates batches of samples
+		# Infinite loop
+		while 1:
+			# Generate order of exploration of dataset
+			indexes = self.__get_exploration_order(list_IDs)
+			# Generate batches
+			imax = int(len(indexes)/self.batch_size)
+			for i in range(imax):
+				# Find list of IDs
+				list_IDs_temp = [list_IDs[k] for k in indexes[i*self.batch_size:(i+1)*self.batch_size]]
+				# Generate data
+				X, X1, y = self.__data_generation(labels, list_IDs_temp)
+				yield ([X, X1], y)
 
-          # Generate batches
-          imax = int(len(indexes)/self.batch_size)
-          for i in range(imax):
-              # Find list of IDs
-              list_IDs_temp = [list_IDs[k] for k in indexes[i*self.batch_size:(i+1)*self.batch_size]]
+	def __get_exploration_order(self, list_IDs):
+		# Generates order of exploration
+		# Find exploration order
+		indexes = np.arange(len(list_IDs))
+		if self.shuffle == True:
+			np.random.shuffle(indexes)
+			
+		return indexes
 
-              # Generate data
-              X, y = self.__data_generation(labels, list_IDs_temp)
+	def __data_generation(self, labels, list_IDs_temp):
+		# Generates data of batch_size samples
+		# X : (n_samples, v_size, v_size, n_channels)
+		# Initialization
+		'''
+		X = np.empty((self.batch_size, self.dim_x, self.dim_y, 1))
+		X1 = np.empty((self.batch_size, self.dim_x1))
+		y = np.empty((self.batch_size), dtype = int)
+		'''
+		# Generate data
+		[X, X1, y] = self.input_data_process.genPassageAndLabels(list_IDs_temp, labels, self.batch_size)
+		'''
+		for i, ID in enumerate(list_IDs_temp):
+			# Store volume
+			#X[i, :, :, 0] = np.load(ID + '_psg.npy')
+			#X1[i, :, :, 0] = np.load(ID + '_h.npy')
+			# Store class
+			y[i] = labels[ID]
+		'''	
+		return X, X1, y
 
-              yield X, y
-
-  def __get_exploration_order(self, list_IDs):
-      'Generates order of exploration'
-      # Find exploration order
-      indexes = np.arange(len(list_IDs))
-      if self.shuffle == True:
-          np.random.shuffle(indexes)
-
-      return indexes
-
-  def __data_generation(self, labels, list_IDs_temp):
-      'Generates data of batch_size samples' # X : (n_samples, v_size, v_size, n_channels)
-      # Initialization
-      X = np.empty((self.batch_size, self.dim_x, self.dim_y, self.dim_z, 1))
-	  X1 = np.empty((self.batch_size, self.dim_x1))
-      y = np.empty((self.batch_size), dtype = int)
-
-      # Generate data
-      for i, ID in enumerate(list_IDs_temp):
-          # Store volume
-          X[i, :, :, 0] = np.load(ID + '_psg.npy')
-		  X1[i, :, :, 0] = np.load(ID + '_h.npy')
-
-          # Store class
-          y[i] = labels[ID]
-
-      return [X, X1], sparsify(y)
-
-def sparsify(y):
-  'Returns labels in binary NumPy array'
-  n_classes = # Enter number of classes
-  return np.array([[1 if y[i] == j else 0 for j in range(n_classes)]
-                   for i in range(y.shape[0])])
+	def __sparsify(self, y):
+		#'Returns labels in binary NumPy array'
+		n_classes = 2# Enter number of classes
+		return np.array([[1 if y[i] == j else 0 for j in range(n_classes)] for i in range(y.shape[0])])
--- Neural-Passage-Model/preprocess.py
+++ Neural-Passage-Model/preprocess.py
@@ -2,23 +2,19 @@ import sys
 sys.path.append("../Tools")
 
 import numpy as np
-import gc
 import ProcDoc
 
-data = {}                # content of document (doc, content)
-background_model = {}    # word count of 2265 document (word, number of words)
-query = {}                # query
-
 class InputDataProcess(object):
-	def __init__(self, query_path = None, document_path = None, corpus = "TDT2"):
+	def __init__(self, num_of_heter_feats = 10, max_qry_length = 1794, max_doc_length = 2907, query_path = None, document_path = None, corpus = "TDT2"):
 		resPos = True
-		self.max_qry_length = 1794
-		self.max_doc_length = 2907
+		self.max_qry_length = max_qry_length
+		self.max_doc_length = max_doc_length
+		self.num_of_heter_feats = num_of_heter_feats
 		if query_path == None: 
 			query_path = "../Corpus/" + corpus + "/Train/XinTrainQryTDT2/QUERY_WDID_NEW"
 		if document_path == None:
 			document_path = "../Corpus/" + corpus + "/SPLIT_DOC_WDID_NEW"
-		# read document, reverse position
+		# read document, reserve position
 		doc = ProcDoc.read_file(document_path)
 		self.doc = ProcDoc.doc_preprocess(doc, resPos)
 
@@ -28,34 +24,66 @@ class InputDataProcess(object):
 		
 		# HMMTrainingSet
 		self.hmm_training_set = ProcDoc.read_relevance_dict()
-		self.heter_feature = __genFeature()
+		self.heter_feature = self.__genFeature(num_of_heter_feats)
 		
-	def genPassage(self, IDs_list, batch_size):
+	def genPassageAndLabels(self, list_IDs, labels, batch_size):
 		qry = self.qry
 		doc = self.doc
 		max_qry_length = self.max_qry_length
 		max_doc_length = self.max_doc_length
 		heter_feature = self.heter_feature
-		pas_mat_batch = []
-		heter_feature_batch = []
-		rel_batch = []
+		num_of_heter_feats = self.num_of_heter_feats
+		psg_mat_batch = np.zeros((batch_size, max_qry_length, max_doc_length, 1))
+		heter_feature_batch = np.zeros((batch_size, num_of_heter_feats))
+		rel_batch = np.zeros((batch_size))
 		# generate passage
-		for data_ID in IDs_list:
+		for idx, data_ID in enumerate(list_IDs):
 			[q_id, d_id] = data_ID.split("_")
 			q_terms = map(int, qry[q_id].split())
 			d_terms = np.asarray(map(int, doc[d_id].split()))
 			psg_mat = np.asarray([[(q_t == d_terms)] for q_t in q_terms]).reshape(len(q_terms), len(d_terms), 1)
-			psg_mat = self.__mergeMat(np.zeros((qry_length, doc_length, 1)), psg_mat)
-			psa_mat_batch.append(np.copy(psg_mat))
-			heter_feature_batch.append(heter_feature[q_id])
-			
-		return [np.asarray(psa_mat_batch), np.asarray(heter_feature_batch)]
+			psg_mat = self.__mergeMat(np.zeros((max_qry_length, max_doc_length, 1)), psg_mat)
+			psg_mat_batch[idx] = np.copy(psg_mat)
+			heter_feature_batch[idx] = heter_feature[q_id]
+			rel_batch[idx] = labels[data_ID]
+		return [np.asarray(psg_mat_batch), np.asarray(heter_feature_batch), np.asarray(rel_batch)]
 	
-	def __genFeature(self):
-		######################## TODO ######################
+	def genTrainValidSet(self):
+		print "generate training set and validation set"
 		qry = self.qry
 		doc = self.doc
-		return np.random.rand(800, 10)
+		total_qry = len(qry.keys())
+		total_doc = len(doc.keys())
+		hmm_training_set = self.hmm_training_set
+		labels = {}
+		total = total_qry * total_doc
+		num_of_train = total * 8 / 10 
+		num_of_valid = total - num_of_train
+		partition = {'train': [], 'validation': []}
+		# relevance between queries and documents
+		for q_id in qry:
+			for d_id in doc:
+				if d_id in hmm_training_set[q_id]:
+					labels[q_id + "_" + d_id] = 1
+				else:
+					labels[q_id + "_" + d_id] = -1
+		# partition
+		ID_list = labels.keys()
+		# shuffle
+		np.random.shuffle(ID_list)
+		partition['train'] = [id for id in ID_list[:num_of_train]]
+		partition['validation'] = [id for id in ID_list[num_of_train:]]
+		return [partition, labels]
+	
+	def __genFeature(self, num_of_heter_feats):
+		###################### TODO ######################
+		print "generate h features"
+		qry = self.qry
+		doc = self.doc
+		heter_feats = {}
+		for q_id in qry.keys():
+			heter_feats[q_id] = np.random.rand(num_of_heter_feats)
+		return heter_feats
 		
 	def __mergeMat(self, b1, b2, pos = [0, 0]):
 		[pos_v, pos_h] = pos
@@ -65,27 +93,5 @@ class InputDataProcess(object):
 		h_range2 = slice(max(0, -pos_h), min(-pos_h + b1.shape[1], b2.shape[1]))
 		b1[v_range1, h_range1] += b2[v_range2, h_range2]
 		return b1
-	
-	def genTrainValidSet(self):
-		qry = self.qry
-		doc = seld.doc
-		total_qry = len(qry.keys())
-		total_doc = len(doc.keys())
-		hmm_training_set = self.hmm_training_set
-		labels = {}
-		total = total_qry * total_doc
-		num_of_train = total / 8
-		num_of_valid = total - num_of_train
-		partition = {'train': [], 'validation': []}
-		# relevance between queries and documents
-		for q_id in qry:
-			for d_id in doc:
-				if d_id in hmm_training_set[q_id]:
-					labels[q_id + "_" + d_id] = 1
-				else:
-					labels[q_id + "_" + d_id] = 0
-		# partition
-		ID_list = np.random.shuffle(labels.keys())
-		partition['train'] = [id] for id in ID_list[:num_of_train]
-		partition['validation'] = [id] for id in ID_list[num_of_train:]
-		return [partition, labels]
+
+
