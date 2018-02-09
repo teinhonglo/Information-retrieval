c57836679ed5d0572539ee800fa4c87bc5a6028d
teinhonglo <teinhonglo@gmail.com>
2018-02-09 23:10:51 +0800

    match paper criterion

--- Neural-Passage-Model/NPM.py
+++ Neural-Passage-Model/NPM.py
@@ -8,21 +8,31 @@ from keras.layers.pooling import AveragePooling2D, GlobalMaxPooling2D
 from keras.layers.merge import concatenate, dot
 from keras.models import Model
 
-def create_model(MAX_QRY_LENGTH = 50, MAX_DOC_LENGTH = 2900, NUM_OF_FEATS = 10, PSG_SIZE = 50, NUM_OF_FILTERS = 5, tau = 1):
-	alpha_size = NUM_OF_FILTERS
+def create_model(MAX_QRY_LENGTH = 50, MAX_DOC_LENGTH = 2900, NUM_OF_FEATS = 10, PSGS_SIZE = [(50, 1)], NUM_OF_FILTERS = 5, tau = 1):
+	alpha_size = len(PSGS_SIZE)
 	psgMat = Input(shape = (MAX_QRY_LENGTH, MAX_DOC_LENGTH, 1,), name="passage")
 	homoMat = Input(shape = (NUM_OF_FEATS, ), name="h_feats")
 	# Convolution2D, Meaning pooling and Max pooling.
 	# Conv2D, Mean pooling, Max pooling
-    #psgMat_ZP  = ZeroPadding2D()(psgMat)
-	M = Convolution2D(filters=NUM_OF_FILTERS, kernel_size=(1, PSG_SIZE), strides=tau, padding='valid', name="pConv2D")(psgMat)
-	K = AveragePooling2D(pool_size=(MAX_QRY_LENGTH, 1), strides=1, name="pAvePool")(M)
-	r = GlobalMaxPooling2D(name="pMaxPool")(K)
+	M, K, r = [], [], []
+	for idx, PSG_SIZE in enumerate(PSGS_SIZE):
+		tau = PSG_SIZE[0] / 2
+		pool_size = (MAX_QRY_LENGTH - PSG_SIZE[0]) / tau + 1
+		# Convolution
+		m_1 = Convolution2D(filters=NUM_OF_FILTERS, kernel_size=PSG_SIZE, strides=tau, padding='valid', name="pConv2D_" + str(idx))(psgMat)
+		M.append(m_1)
+		# Mean pooling
+		k_1 = AveragePooling2D(pool_size=(pool_size, 1), strides=1, name="pAvePool_" + str(idx))(M[idx])
+		K.append(k_1)
+		# Max Pooling
+		r_1 = GlobalMaxPooling2D(name="pMaxPool_" + str(idx))(K[idx])
+		r.append(r_1)
+	concat_r = concatenate(r)
 	# Fusion Matrix and predict relevance
 	# get h(q, d)
 	# MLP(DENSE(len(r(q,d))))
 	phi_h = Dense(alpha_size, activation="softmax", name="TrainMat")(homoMat)
-	dot_prod = dot([r, phi_h], axes = 1, name="rel_dot")
+	dot_prod = dot([concat_r, phi_h], axes = 1, name="rel_dot")
 	# tanh(dot(r.transpose * h))
 	pred = Activation("tanh", name="activation_tanh")(dot_prod)
 	
@@ -39,17 +49,19 @@ if __name__ == "__main__":
 	MAX_QRY_LENGTH = 1794
 	MAX_DOC_LENGTH = 2907
 	NUM_OF_FEATS = 10
-	PSG_SIZE = 50
-	NUM_OF_FILTERS = 5
+	PSGS_SIZE = [(16, 1), (150, 1)]
+	NUM_OF_FILTERS = 1
 	batch_size = 4
 	tau = 1
 	X = np.random.rand(batch_size, MAX_QRY_LENGTH, MAX_DOC_LENGTH, 1)
 	X1 = np.random.rand(batch_size, NUM_OF_FEATS)
 	y = np.random.rand(batch_size)
-	model = create_model(MAX_QRY_LENGTH, MAX_DOC_LENGTH, NUM_OF_FEATS, PSG_SIZE, NUM_OF_FILTERS, tau)
-	model.compile(	loss= 'kullback_leibler_divergence',	optimizer='Nadam',	metrics=['accuracy'])
+	model = create_model(MAX_QRY_LENGTH, MAX_DOC_LENGTH, NUM_OF_FEATS, PSGS_SIZE, NUM_OF_FILTERS, tau)
+	model.compile(loss= 'kullback_leibler_divergence',	optimizer='Nadam',	metrics=['accuracy'])
+	
 	model.fit([X, X1], y, 
 			batch_size=batch_size, 
 			epochs=10,	
 			verbose=1,	
 			shuffle=True)
+	
