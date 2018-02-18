c457e706e1c87495173d80a20909c659e12d8459
teinhonglo <teinhonglo@gmail.com>
2018-02-18 16:53:06 +0800

    return sequence is better

--- Neural-Passage-Model/addtion_rnn.py
+++ Neural-Passage-Model/addtion_rnn.py
@@ -165,18 +165,18 @@ model = Sequential()
 # "Encode" the input sequence using an RNN, producing an output of HIDDEN_SIZE.
 # Note: In a situation where your input sequences have a variable length,
 # use input_shape=(None, num_feature).
-model.add(RNN(HIDDEN_SIZE, input_shape=(MAXLEN, encode_length)))
+model.add(RNN(HIDDEN_SIZE, input_shape=(MAXLEN, encode_length), return_sequences=True))
 # As the decoder RNN's input, repeatedly provide with the last hidden state of
 # RNN for each time step. Repeat 'DIGITS + 1' times as that's the maximum
 # length of output, e.g., when DIGITS=3, max output is 999+999=1998.
-model.add(layers.RepeatVector(MAXLEN))
+#model.add(layers.RepeatVector(MAXLEN))
 # The decoder RNN could be multiple layers stacked or a single layer.
 for _ in range(LAYERS):
 	# By setting return_sequences to True, return not only the last output but
 	# all the outputs so far in the form of (num_samples, timesteps,
 	# output_dim). This is necessary as TimeDistributed in the below expects
 	# the first dimension to be the timesteps.
-	model.add(RNN(HIDDEN_SIZE, return_sequences=True))
+	model.add(RNN(HIDDEN_SIZE, activation='sigmoid', return_sequences=True))
 
 # Apply a dense layer to the every temporal slice of an input. For each of step
 # of the output sequence, decide which character should be chosen.
