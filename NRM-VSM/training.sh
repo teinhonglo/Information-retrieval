296b5aa934aa1ad54a0ffa8e206a321315781ecf
teinhonglo <teinhonglo@gmail.com>
2018-03-22 22:52:01 +0800

    train args and command line example

---
+++ NRM-VSM/training.sh
@@ -0,0 +1,2 @@
+python local/VSM.py --qry_dataset ../Corpus/TDT2/QUERY_WDID_NEW --rel_dataset ../Corpus/TDT2/AssessmentTrainSet/AssessmentTrainSet.txt --data_storage data/Test --is_train False
+python local/VSM.py --qry_dataset ../Corpus/TDT2/Train/XinTrainQryTDT2/QUERY_WDID_NEW --rel_dataset ../Corpus/TDT2/Train/QDRelevanceTDT2_forHMMOutSideTrain --data_storage data/Train --is_train True
