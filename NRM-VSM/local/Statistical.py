22a66a3f778112013aa3202ddd67bedaf905341a
zweiein <evangelinathanasiakittymcdowel@gmail.com>
2018-03-22 11:53:00 +0800

    doc freq

--- NRM-VSM/local/Statistical.py
+++ NRM-VSM/local/Statistical.py
@@ -5,9 +5,9 @@ import types
 def TFIDF(qry, doc):
     doc_freq = docFreq(doc)
     num_docs = len(list(doc.keys())) + 1
-    qry_new = {q_id : {q_wid : (1 + np.log(q_wc)) * np.log(num_docs / (1 + doc_freq[q_wid][0])) 
+    qry_new = {q_id : {q_wid : (np.log(q_wc)) * np.log(num_docs / (1 + doc_freq[q_wid][0])) 
                 for q_wid, q_wc in q_content.items()} for q_id, q_content in qry.items()}
-    doc_new = {d_id : {d_wid : (1 + np.log(d_wc)) * np.log(num_docs / (1 + doc_freq[d_wid][0])) 
+    doc_new = {d_id : {d_wid : (np.log(d_wc)) * np.log(num_docs / (1 + doc_freq[d_wid][0])) 
                 for d_wid, d_wc in d_content.items()} for d_id, d_content in doc.items()}
     return qry_new, doc_new
 
@@ -16,7 +16,7 @@ def docFreq(doc, vocab_size = 51253):
     for name, word_list in doc.items():
         temp_word_list = {}
         # assume type of word_list is dictionary
-        for word, word_count in temp_word_list.items():
+        for word, word_count in word_list.items():
             corpus_dFreq_total[int(word), 0] += 1
             corpus_dFreq_total[int(word), 1] += word_count
     return corpus_dFreq_total
@@ -26,6 +26,6 @@ def docLen(doc):
     for d_id, d_cont in doc.items():
         doc_len = 0
         for d_wid, d_wc in d_cont.items():
-            doc_len += d_wc
-        docs_len[d_id] = d_wc
-	return docs_len    
+            doc_len += d_wc  
+        docs_len[d_id] = doc_len
+    return docs_len    
