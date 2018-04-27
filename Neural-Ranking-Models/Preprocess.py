7ac1a2662e268de4a2b07f6934b1daea31ce6374
teinhonglo <teinhonglo@gmail.com>
2018-04-27 23:01:34 +0800

    experiment

---
+++ Neural-Ranking-Models/Preprocess.py
@@ -0,0 +1,176 @@
+import sys
+sys.path.append("../Tools")
+import numpy as np
+np.random.seed(5566)
+from sklearn import preprocessing
+import ProcDoc
+
+class InputDataProcess(object):
+    def __init__(self, num_of_homo_feats = 10, max_qry_length = 1794, max_doc_length = 2907, query_path = None, document_path = None, corpus = "TDT2"):
+        res_pos = True
+        self.num_vocab = 51253
+        self.max_qry_length = max_qry_length
+        self.max_doc_length = max_doc_length
+        self.num_of_homo_feats = num_of_homo_feats
+        if query_path == None: 
+            query_path = "../Corpus/" + corpus + "/Train/XinTrainQryTDT2/QUERY_WDID_NEW"
+        if document_path == None:
+            document_path = "../Corpus/" + corpus + "/SPLIT_DOC_WDID_NEW"
+        
+        # relevance set
+        self.hmm_training_set = ProcDoc.readRELdict()
+        
+	# read document, reserve position
+        doc = ProcDoc.readFile(document_path)
+        self.doc = ProcDoc.docPreproc(doc, res_pos, 200)
+		
+        # read query, reserve position
+        qry = ProcDoc.readFile(query_path)
+        self.qry = ProcDoc.qryPreproc(qry, self.hmm_training_set, res_pos, 200)        
+        
+        # generate h featrues
+        self.homo_feats = self.__genFeature(num_of_homo_feats)
+        
+    def genPassageAndLabels(self, list_IDs, labels, batch_size):
+        qry = self.qry
+        doc = self.doc
+        max_qry_length = self.max_qry_length
+        max_doc_length = self.max_doc_length
+        homo_feats = self.homo_feats
+        num_of_homo_feats = self.num_of_homo_feats
+        psg_mat_batch = np.zeros((batch_size, max_qry_length, max_doc_length, 1))
+        homo_feats_batch = np.zeros((batch_size, num_of_homo_feats))
+        rel_batch = np.zeros((batch_size))
+        weight_batch = np.zeros((batch_size))
+		
+        # generate passage
+        for idx, data_ID in enumerate(list_IDs):
+            [q_id, d_id] = data_ID.split("_")
+            q_terms = qry[q_id]
+            d_terms = np.asarray(doc[d_id])
+            psg_mat = np.asarray([[(q_t == d_terms)] for q_t in q_terms]).reshape(len(q_terms), len(d_terms), 1)
+            psg_mat = self.__mergeMat(np.zeros((max_qry_length, max_doc_length, 1)), psg_mat)
+            # print psg_mat.shape
+            psg_mat_batch[idx] = np.copy(psg_mat)
+            homo_feats_batch[idx] = homo_feats[q_id]
+            rel_batch[idx] = labels[data_ID][0]
+            weight_batch[idx] = labels[data_ID][1]
+        return [psg_mat_batch, homo_feats_batch, rel_batch, weight_batch]
+    
+    def genTrainValidSet(self, percent = None, isTest = False):
+        print "generate training set and validation set"
+        if percent == None: percent = 80
+        qry = self.qry
+        doc = self.doc
+        total_qry = len(qry.keys())
+        total_doc = len(doc.keys())
+        hmm_training_set = self.hmm_training_set
+        labels = {}
+        partition = {'train': [], 'validation': []}
+        part_answer = {'train': [], 'validation': []}
+        # sample weights
+        num_qry_rel = {q_id: len(hmm_training_set[q_id]) for q_id in qry}
+        n_classes = len(qry.keys())
+        n_samples = sum(num_qry_rel.values()) * 1.0
+        sample_weights = {q_id: np.log(1 + n_samples / (n_classes * q_n_rel)) for q_id, q_n_rel in num_qry_rel.items()}
+		
+        # relevance between queries and documents
+        for q_id in qry:
+            flag = 0
+            for d_id in doc:
+                if d_id in hmm_training_set[q_id]:
+                    labels[q_id + "_" + d_id] = [1, sample_weights[q_id]]
+                    flag += 1
+                else:
+                    if flag > 0 or isTest:	
+                        labels[q_id + "_" + d_id] = [-1, sample_weights[q_id]]
+                        flag -= 1
+        # partition
+        ID_list = labels.keys()
+        total = len(ID_list)
+        num_of_train = total * percent / 100
+        num_of_valid = total - num_of_train
+        # shuffle
+        np.random.shuffle(ID_list)
+        partition['train'] = [id for id in ID_list[:num_of_train]]
+        part_answer['train'] = [labels[id][0] for i, id in enumerate(ID_list[:num_of_train])]
+        #[partition['train'], part_answer['train']] = self.__balancedSubsample(partition['train'], part_answer['train'], labels)
+        partition['validation'] = [id for id in ID_list[num_of_train:]]
+        part_answer['validation'] = [labels[id][0] for i, id in enumerate(ID_list[num_of_train:])]
+        #[partition['validation'], part_answer['validation']] = self.__balancedSubsample(partition['validation'], part_answer['validation'], labels)
+        return [partition, labels, part_answer]
+    
+    def __genFeature(self, num_of_homo_feats):
+        print "generate h features"
+        qry = self.qry
+        doc = self.doc
+        homo_feats = {}
+        df = ProcDoc.docFreq(doc)
+        
+        for q_id, q_terms in qry.items():
+            npscq = np.asarray([self.__scq(df, q_term) for q_term in q_terms])
+            harm_mean = self.__harmMean(npscq)
+            geo_mean = self.__geoMean(npscq)
+            homo_feats[q_id] = np.asarray([np.std(npscq), np.sum(npscq), np.amax(npscq), np.amin(npscq), np.mean(npscq), harm_mean, geo_mean])
+        return homo_feats
+		
+    def __scq(self, df, term):
+        eps = np.finfo(float).eps
+        num_docs = len(self.doc.keys())
+        # print df[term, 1],1 + self.num_vocab/(eps + df[term, 0])
+        return (1 + np.log(1 + df[term, 1])) * np.log(1 + num_docs/(eps + df[term, 0]))
+    
+    def __harmMean(self, a):
+        return len(a) / np.sum(1.0 / a)
+    	
+    def __geoMean(self, a):
+        a = np.log(a)
+        return np.exp(a.sum() * 1.0 / len(a))
+    
+    def __mergeMat(self, b1, b2, pos = [0, 0]):
+        [pos_v, pos_h] = pos
+        v_range1 = slice(max(0, pos_v), max(min(pos_v + b2.shape[0], b1.shape[0]), 0))
+        h_range1 = slice(max(0, pos_h), max(min(pos_h + b2.shape[1], b1.shape[1]), 0))
+        v_range2 = slice(max(0, -pos_v), min(-pos_v + b1.shape[0], b2.shape[0]))
+        h_range2 = slice(max(0, -pos_h), min(-pos_h + b1.shape[1], b2.shape[1]))
+        b1[v_range1, h_range1] += b2[v_range2, h_range2]
+        return b1
+    
+    def __balancedSubsample(self, x, y, labels):
+        print "balanced sample : Oversampling"
+        uni_class = {}
+        xs = list(x)
+        ys = list(y)
+        for ans in y:
+            if ans in uni_class:
+                uni_class[ans] += 1
+            else:
+                uni_class[ans] = 1
+        isBanlanced = (uni_class[0] == uni_class[1])
+        isNegLarger = (uni_class[0] > uni_class[1])
+        if not isBanlanced:
+            if isNegLarger:
+                i = 0
+                while i < (uni_class[0] - uni_class[1]):
+                    for i_x, id in enumerate(x):
+                        if y[i_x] == 1:
+                            xs.append(id)
+                            ys.append(1)
+                            i += 1
+                        else:	
+                            continue
+            if isNegLarger:
+                i = 0
+                while i < (uni_class[1] - uni_class[0]):
+                    for i_x, id in enumerate(x):
+                        if y[i_x] == 0:
+                            xs.append(id)
+                            ys.append(0)
+                            i += 1
+                        else:	
+                            continue				
+        return xs, ys
+if __name__ == "__main__":
+    a = InputDataProcess()
+
+
---
+++ Neural-Ranking-Models/Testing.py
@@ -0,0 +1,73 @@
+import numpy as np
+np.random.seed(5566)
+import os
+import tensorflow as tf
+from keras.callbacks import ModelCheckpoint
+from keras.models import load_model
+import NPM
+from SeqGenerator import DataGenerator
+from Preprocess import InputDataProcess
+from Evaluate import EvaluateModel
+from collections import defaultdict
+import operator
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
+sess = tf.Session(config=tf.ConfigProto(gpu_options=tf.GPUOptions(allow_growth=True),
+                  inter_op_parallelism_threads = 1, intra_op_parallelism_threads = 1))
+
+MAX_QRY_LENGTH = 200
+MAX_DOC_LENGTH = 200
+NUM_OF_FEATS = 7
+batch_size = 4
+percent = 100
+optimizer = "Adam"
+loss = "kullback_leibler_divergence"
+exp_path = "exp/"
+test_path = "../Corpus/TDT2/QUERY_WDID_NEW"
+model_name = "SubSampling_categorical_cnn_Adam_categorical_crossentropy_weights-08-0.56.hdf5"
+
+def precision(y_true, y_pred):
+    """Precision metric.
+    Only computes a batch-wise average of precision.
+    Computes the precision, a metric for multi-label classification of
+    how many selected items are relevant.
+    """
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return precision
+
+input_data_process = InputDataProcess(NUM_OF_FEATS, MAX_QRY_LENGTH, MAX_DOC_LENGTH)#, test_path)
+evaluate_model = EvaluateModel("../Corpus/TDT2/Train/QDRelevanceTDT2_forHMMOutSideTrain", True)
+# Parameters
+params = {'input_data_process': input_data_process,
+          'dim_x': MAX_QRY_LENGTH,
+          'dim_y': MAX_DOC_LENGTH,
+		  'dim_x1': NUM_OF_FEATS,
+          'batch_size': batch_size,
+          'shuffle': False}
+
+[partition, labels, partition_answer] = input_data_process.genTrainValidSet(percent)
+
+# Generators
+training_generator = DataGenerator(**params).generate(labels, partition['train'])
+
+# Design model
+model = load_model(exp_path + model_name)
+model.compile(optimizer = optimizer, loss = loss, metrics=["accuracy", precision])
+qry_doc = defaultdict(list)
+with tf.device('/gpu:0'):
+	# Train model on dataset
+	pred = model.evaluate_generator(generator = training_generator,	steps = len(partition['train']) / batch_size, verbose=1)
+	print 
+	'''
+	for idx, id in enumerate(partition['train']):
+		q_id, d_id = id.split('_')
+		qry_doc[q_id].append([d_id, pred[idx]])
+	for id, docs_point in qry_doc.items():
+		qry_doc[id] = sorted(docs_point, key=operator.itemgetter(1), reverse = True)
+	mAP = evaluate_model.mAP(qry_doc)
+	print mAP
+	'''
+# import evaluation
+# other
