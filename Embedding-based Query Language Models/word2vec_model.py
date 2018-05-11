f0de6e43fb52e418e2db07efb58ceb271649a67e
weichengchao <weicheng0912@gmail.com>
2018-05-11 13:45:12 +0800

    bug fix

--- Embedding-based Query Language Models/word2vec_model.py
+++ Embedding-based Query Language Models/word2vec_model.py
@@ -8,63 +8,63 @@ import warnings
 warnings.filterwarnings(action='ignore', category=UserWarning, module='gensim')
 
 class word2vec_model():
-	def __init__(self, alpha = 50, c = 0.7):
-		self.word2vec = self.readWord2VecModel()
+    def __init__(self, alpha = 50, c = 0.7):
+        self.word2vec = self.readWord2VecModel()
         self.vocab = self.word2vec.keys()
-		self.vocabulary_length = len(self.word2vec.keys())
-		first_word = self.word2vec.keys()[0]
-		self.word_vec_length = self.word2vec[first_word].shape[0]
-		self.alpha = alpha
-		self.c = c
-		self.mean_vector = self.calcMeanVec()
-		
-	def readWord2VecModel(self):
-		word2vec = []
-		with open("../Corpus/word2vec.pickle", "rb") as file:
-			word2vec = Pickle.load(file)
-		#word2vec = word2vec.wv
-		return word2vec
-	
-	def calcMeanVec(self):
-		w2v = self.word2vec
-		w2v_vocab = self.vocab
-		mean_vector = np.zeros(self.word_vec_length)
-		length = self.vocabulary_length
-		for word in w2v_vocab:
-			mean_vector += w2v[word]
-		mean_vector /= self.vocabulary_length
-		return mean_vector
+        self.vocabulary_length = len(self.word2vec.keys())
+        first_word = self.word2vec.keys()[0]
+        self.word_vec_length = self.word2vec[first_word].shape[0]
+        self.alpha = alpha
+        self.c = c
+        self.mean_vector = self.calcMeanVec()
+        
+    def readWord2VecModel(self):
+        word2vec = []
+        with open("../Corpus/word2vec_dict.pkl", "rb") as file:
+            word2vec = Pickle.load(file)
+        #word2vec = word2vec.wv
+        return word2vec
+    
+    def calcMeanVec(self):
+        w2v = self.word2vec
+        w2v_vocab = self.vocab
+        mean_vector = np.zeros(self.word_vec_length)
+        length = self.vocabulary_length
+        for word in w2v_vocab:
+            mean_vector += w2v[word]
+        mean_vector /= self.vocabulary_length
+        return mean_vector
 
-	def sumOfTotalSimilarity(self, cur_set, collection):
-		# avoid memory error
-		total_similarity ={}
-		
-		for word, cur_word_vec in cur_set.items():
-			total_similarity[word] = 0
-			word_sq_vec = np.array(collection.values())
-			cosine_vectors = (cur_word_vec * word_sq_vec).sum(axis = 1)
-			for cosine_result in cosine_vectors:
-				total_similarity[word] += self.sigmoid(cosine_result)
-		return total_similarity		
-	
-	def getWordSimilarity(self, w1_vec, w2_vec):
-		cosine_result = (w1_vec* w2_vec).sum(axis = 0)
-		return self.sigmoid(cosine_result)
-	
-	def sigmoid(self, x):
-		gamma = -self.alpha * (x - self.c)
-		return 1 / (1 + exp(gamma))
-		
-	def setAlpha(self, a):
-		self.alpha = a
-		
-	def getWord2Vec(self):
-		return self.word2vec	
+    def sumOfTotalSimilarity(self, cur_set, collection):
+        # avoid memory error
+        total_similarity ={}
+        
+        for word, cur_word_vec in cur_set.items():
+            total_similarity[word] = 0
+            word_sq_vec = np.array(collection.values())
+            cosine_vectors = (cur_word_vec * word_sq_vec).sum(axis = 1)
+            for cosine_result in cosine_vectors:
+                total_similarity[word] += self.sigmoid(cosine_result)
+        return total_similarity        
+    
+    def getWordSimilarity(self, w1_vec, w2_vec):
+        cosine_result = (w1_vec* w2_vec).sum(axis = 0)
+        return self.sigmoid(cosine_result)
+    
+    def sigmoid(self, x):
+        gamma = -self.alpha * (x - self.c)
+        return 1 / (1 + exp(gamma))
+        
+    def setAlpha(self, a):
+        self.alpha = a
+        
+    def getWord2Vec(self):
+        return self.word2vec    
     
     def getVocab(self):
-		return self.vocab    
-	
-	def getMeanVec(self):
-		return self.mean_vector	
-	
+        return self.vocab    
+    
+    def getMeanVec(self):
+        return self.mean_vector    
+    
 
