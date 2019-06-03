a33feec0e65f2d10d25fbfbf5c86d3897230a39a
teinhonglo <teinhonglo@gmail.com>
2019-06-03 11:26:07 +0800

    add spoken options

--- example/create_rel_irrel.py
+++ example/create_rel_irrel.py
@@ -6,25 +6,42 @@ sys.path.append("../Tools")
 
 import ProcDoc
 import Evaluate
+import os
 
-is_training = False
+is_training = True
 is_short = False
+is_spoken = False
+
+output_dir = "data"
 output_name = ""
+
+if not os.path.isdir(output_dir):
+    os.mkdir(output_dir, 0755)
 
 if is_training:
     qry_path = "../Corpus/TDT2/Train/XinTrainQryTDT2/QUERY_WDID_NEW"
     rel_path = "../Corpus/TDT2/Train/QDRelevanceTDT2_forHMMOutSideTrain"
-    output_name = "train.txt"
+    output_name = "train"
 else:
     if is_short:
         qry_path = "../Corpus/TDT2/QUERY_WDID_NEW_middle"
-        output_name = "test_short.txt"
+        output_name = "test_short"
     else:
         qry_path = "../Corpus/TDT2/QUERY_WDID_NEW"
-        output_name = "test_long.txt"
+        output_name = "test_long"
     rel_path = "../Corpus/TDT2/AssessmentTrainSet/AssessmentTrainSet.txt"
 
-doc_path = "../Corpus/TDT2/SPLIT_DOC_WDID_NEW"
+output_eval_name = output_name + ".all"
+
+if is_spoken:
+    doc_path = "../Corpus/TDT2/Spoken_Doc"
+    output_name = output_name + ".spk.csv"
+    output_eval_name = output_eval_name + ".spk.csv"
+else:
+    doc_path = "../Corpus/TDT2/SPLIT_DOC_WDID_NEW"
+    output_name = output_name + ".csv"
+    output_eval_name = output_eval_name + ".csv"
+
 dict_path = "../Corpus/TDT2/LDC_Lexicon.txt"
 ID_map = {}
 
@@ -57,9 +74,12 @@ qry_mdl_dict = ID2Word(qry_mdl_dict, ID_map)
 doc_mdl_dict = ID2Word(doc_mdl_dict, ID_map)
 docs_list = doc_mdl_dict.keys()
 
+print(output_dir + "/" + output_name, output_dir + "/" + output_eval_name)
+
 # create relevant and irrelevant set
-with codecs.open(output_name, "w", encoding="utf-8") as wf:
+with codecs.open(output_dir + "/" + output_name, "w", encoding="utf-8") as wf:
     # iter the relevant set
+    wf.write("qry,qry_content,doc,doc_content,rel\n")
     for qry, q_rel_docs in rel_set.items():
         for doc in q_rel_docs:
             # ===================REL DOCS====================
@@ -68,14 +88,12 @@ with codecs.open(output_name, "w", encoding="utf-8") as wf:
             wf.write(",")
             for qry_word in qry_mdl_dict[qry]:
                 wf.write(qry_word)
-                wf.write(" ")
             wf.write(",")
             wf.write(doc)
             wf.write(",")
             # irrelvant docs
             for doc_word in doc_mdl_dict[doc]:
                 wf.write(doc_word)
-                wf.write(" ")
             wf.write(",")
             wf.write("1\n")
             # ===================IRREL DOCS====================
@@ -84,7 +102,6 @@ with codecs.open(output_name, "w", encoding="utf-8") as wf:
             wf.write(",")
             for qry_word in qry_mdl_dict[qry]:
                 wf.write(qry_word)
-                wf.write(" ")
             wf.write(",")
             # irrelvant docs
             choice = -1
@@ -95,6 +112,28 @@ with codecs.open(output_name, "w", encoding="utf-8") as wf:
             wf.write(",")
             for doc_word in doc_mdl_dict[r_doc]:
                 wf.write(doc_word)
-                wf.write(" ")
             wf.write(",")
             wf.write("0\n")
+
+if not is_training:
+    with codecs.open(output_dir + "/" + output_eval_name, "w", encoding="utf-8") as wf:
+        # iter the relevant set
+        wf.write("qry,qry_content,doc,doc_content,rel\n")
+        for qry, q_rel_docs in rel_set.items():
+            for doc in docs_list:
+                # prepare prefix query
+                wf.write(qry)
+                wf.write(",")
+                for qry_word in qry_mdl_dict[qry]:
+                    wf.write(qry_word)
+                wf.write(",")
+                wf.write(doc)
+                wf.write(",")
+                # irrelvant docs
+                for doc_word in doc_mdl_dict[doc]:
+                    wf.write(doc_word)
+                wf.write(",")
+                if doc in q_rel_docs:
+                    wf.write("1\n")
+                else:
+                    wf.write("0\n")
