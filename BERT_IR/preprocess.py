59adfef8a69fd84dc21821898067f79c3a6fecbd
teinhonglo <teinhonglo@gmail.com>
2019-06-15 19:05:34 +0800

    cosmetic

--- BERT_IR/preprocess.py
+++ BERT_IR/preprocess.py
@@ -3,13 +3,14 @@
 import numpy as np
 import sys
 sys.path.append("../Tools")
+np.random.seed(10)
 
 import ProcDoc
 import Evaluate
 import os
 
 is_training = False
-is_short = True
+is_short = False
 is_spoken = False
 task_name = "TDT2"
 
@@ -17,7 +18,7 @@ output_name = ""
 output_dir = "data/" + task_name
 
 if not os.path.isdir(output_dir):
-    os.makedirs(output_dir, 0755)
+    os.makedirs(output_dir)
 
 if is_training:
     qry_path = "../Corpus/TDT2/Train/XinTrainQryTDT2/QUERY_WDID_NEW"
@@ -36,8 +37,8 @@ output_eval_name = output_name + ".all"
 
 if is_spoken:
     doc_path = "../Corpus/TDT2/Spoken_Doc"
-    output_name = output_name + ".spk.csv"
-    output_eval_name = output_eval_name + ".spk.csv"
+    output_name = output_name + "_spk.csv"
+    output_eval_name = output_eval_name + "_spk.csv"
 else:
     doc_path = "../Corpus/TDT2/SPLIT_DOC_WDID_NEW"
     output_name = output_name + ".csv"
@@ -77,7 +78,11 @@ docs_list = doc_mdl_dict.keys()
 
 print(output_dir + "/" + output_name, output_dir + "/" + output_eval_name)
 
-# create relevant and irrelevant set
+def write_content_to_file(wf, key, mdl_dict):
+    for word in mdl_dict[key]:
+        wf.write(word)
+
+# create relevant and irrelevant set (pointwise)
 with codecs.open(output_dir + "/" + output_name, "w", encoding="utf-8") as wf:
     # iter the relevant set
     wf.write("qry,qry_content,doc,doc_content,rel\n")
@@ -85,37 +90,30 @@ with codecs.open(output_dir + "/" + output_name, "w", encoding="utf-8") as wf:
         for doc in q_rel_docs:
             # ===================REL DOCS====================
             # prepare prefix query
-            wf.write(qry)
-            wf.write(",")
+            wf.write(qry + ",")
             for qry_word in qry_mdl_dict[qry]:
                 wf.write(qry_word)
-            wf.write(",")
-            wf.write(doc)
-            wf.write(",")
-            # irrelvant docs
+            wf.write("," + doc + ",")
+            # relevant docs
             for doc_word in doc_mdl_dict[doc]:
                 wf.write(doc_word)
-            wf.write(",")
-            wf.write("1\n")
+            wf.write(",1\n")
             # ===================IRREL DOCS====================
             # prepare prefix query
-            wf.write(qry)
-            wf.write(",")
+            wf.write(qry + ",")
             for qry_word in qry_mdl_dict[qry]:
                 wf.write(qry_word)
             wf.write(",")
-            # irrelvant docs
+            # irrelevant docs
             choice = -1
             while choice == -1 or docs_list[choice] in q_rel_docs:
                 choice = np.random.randint(len(docs_list), size=1)[0]
             r_doc = docs_list[choice]
-            wf.write(r_doc)
-            wf.write(",")
+            wf.write(r_doc + ",")
             for doc_word in doc_mdl_dict[r_doc]:
                 wf.write(doc_word)
-            wf.write(",")
-            wf.write("0\n")
-
+            wf.write(",0\n") 
+            
 if not is_training:
     with codecs.open(output_dir + "/" + output_eval_name, "w", encoding="utf-8") as wf:
         # iter the relevant set
@@ -123,13 +121,10 @@ if not is_training:
         for qry, q_rel_docs in rel_set.items():
             for doc in docs_list:
                 # prepare prefix query
-                wf.write(qry)
-                wf.write(",")
+                wf.write(qry + ",")
                 for qry_word in qry_mdl_dict[qry]:
                     wf.write(qry_word)
-                wf.write(",")
-                wf.write(doc)
-                wf.write(",")
+                wf.write("," + doc + ",")
                 # irrelvant docs
                 for doc_word in doc_mdl_dict[doc]:
                     wf.write(doc_word)
@@ -138,3 +133,42 @@ if not is_training:
                     wf.write("1\n")
                 else:
                     wf.write("0\n")
+
+'''
+num_ir_docs = 3
+num_ir_repeats = 3
+# create relevant and irrelevant set (pointwise)
+with codecs.open(output_dir + "/pairwise." + output_name, "w", encoding="utf-8") as wf:
+    # iter the relevant set
+    wf.write("tdt2_id,qry_content,doc_0,doc_1,doc_2,doc_3,label\n")
+    for qry, q_rel_docs in rel_set.items():
+        for doc in q_rel_docs:
+            # prepare prefix query (doc = relevant doc)
+            tdt2_id = qry + "#" + doc 
+            # irrelvant docs
+            ir_docs = []
+            while len(ir_docs) < num_ir_docs * num_ir_repeats:
+                choice = -1
+                while (choice == -1) or (docs_list[choice] in q_rel_docs) or (choice in ir_docs):
+                    choice = np.random.randint(len(docs_list), size=1)[0]
+                ir_docs.append(choice)
+            # preparing qry, doc, and irrel docs
+            for start_idx in range(num_ir_repeats):
+                start_offset = num_ir_repeats * start_idx
+                tmp_tdt2_id = tdt2_id
+                # a relevant document
+                tmp_docs = [doc]
+                # irrelvant documents
+                for current_idx in range(num_ir_docs):
+                   tmp_tdt2_id += "#" + docs_list[start_offset + current_idx]
+                   tmp_docs.append(docs_list[start_offset + current_idx])
+                
+                wf.write(tmp_tdt2_id)
+                wf.write(",")
+                write_content_to_file(wf, qry, qry_mdl_dict)
+                wf.write(",")
+                for doc_name in tmp_docs:
+                    write_content_to_file(wf, doc_name, doc_mdl_dict)
+                    wf.write(",")
+                wf.write("0\n")
+'''
